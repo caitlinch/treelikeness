da05adb3f0974758fcc75f278b1aab356a435918
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-04-03 13:53:19 +1000

    Reduced function parameters to make functions easier to use

--- code/test_statistic.R
+++ code/test_statistic.R
@@ -1,23 +1,23 @@
 # R functions to calculate 3 test statistics for quantifying tree likeness
 
 # Function to call IQ-tree and return a pairwise distance matrix
-iqtree.pdm <- function(iqpath,path,file){
+iqtree.pdm <- function(iqpath,path){
   # Check if the tree file already exists and if it doesn't, run IQ-tree and create it
-  if (file.exists(paste0(path,file,".treefile")) == FALSE){
+  if (file.exists(paste0(path,".treefile")) == FALSE){
     # Given an alignment, get a tree from IQ-tree and find the sum of the pairwise distance matrix
-    system(paste0(iqpath," -s ",path,file," -nt AUTO -redo")) # call IQ-tree!
+    system(paste0(iqpath," -s ",path," -nt AUTO -redo")) # call IQ-tree!
   }
   # Now the tree has been created, open it up
-  tree <- read.tree(paste0(path,file,".treefile")) # read in the tree created in IQ-tree
+  tree <- read.tree(paste0(path,".treefile")) # read in the tree created in IQ-tree
   pdm <- cophenetic.phylo(tree) # get pairwise distance matrix for the taxa - note that this mirrors across the diagonal! (doesn't have half filled with 0's)
   pdm[upper.tri(pdm)] <- 0 # Get upper triangle and replace upper triangle coordinates (TRUE) with 0
   return(pdm)
 }
 
 # Function to open a maximum likelihood distances file from IQ tree and return a pairwise distance matrix
-mldist.pdm <- function(iqpath,path,file){
+mldist.pdm <- function(iqpath,path){
   # Open the maximum likelihood distances file output when creating the tree in IQ-tree
-  filename <- paste0(path,file,".mldist")
+  filename <- paste0(path,".mldist")
   pdm <- read.table(filename,header=FALSE,skip=1) # read in mldist file
   pdm <- pdm[2:ncol(pdm)] # remove first column (taxa names)
   pdm[upper.tri(pdm)] <- 0 # replace upper triangle coordinates (TRUE) with 0
@@ -31,14 +31,14 @@ normalise.matrix <- function(matrix){
 }
 
 # Test statistic based on dividing sum of values in tree pairwise distance matrix by sum of values in alignment matrix
-pdm.ratio <- function(iqpath,path,file){
-  tree_pdm <- iqtree.pdm(iqpath,path,file)
+pdm.ratio <- function(iqpath,path){
+  tree_pdm <- iqtree.pdm(iqpath,path)
   #print(tree_pdm) # to check
   tree_sum <- sum(tree_pdm) # sum up all the pairwise distances
   #print(tree_sum) # print tree sum
   
   # Calculate pairwise distances from the alignment
-  alignment_pdm <- mldist.pdm(iqpath,path,file)
+  alignment_pdm <- mldist.pdm(iqpath,path)
   #print(mldist_pdm) # to check
   alignment_sum <- sum(alignment_pdm) # sum all the pairwise distances
   #print(mldist_sum) # print tree sum
@@ -50,9 +50,9 @@ pdm.ratio <- function(iqpath,path,file){
 
 # Test statistic based on dividing sum of values in tree pairwise distance matrix by sum of values in alignment matrix
 # Adjust entries of alignment matrix and tree matrix to sum to one before dividing, divide result by number of entries in lower-triangle of matrix
-normalised.pdm.ratio <- function(iqpath,path,file){
+normalised.pdm.ratio <- function(iqpath,path){
   #print("call")
-  tree_pdm <- iqtree.pdm(iqpath,path,file)
+  tree_pdm <- iqtree.pdm(iqpath,path)
   #print(tree_pdm) # to check
   tree_pdm <- normalise.matrix(tree_pdm) # adjust matrix so all entries sum to 1
   #print(tree_pdm) # print scaled pdm matrix
@@ -61,7 +61,7 @@ normalised.pdm.ratio <- function(iqpath,path,file){
   
   # Calculate pairwise distances from the alignment
   # Open the maximum likelihood distances file output when creating the tree in IQ-tree
-  alignment_pdm <- mldist.pdm(iqpath,path,file)
+  alignment_pdm <- mldist.pdm(iqpath,path)
   #print(alignment_pdm) # to check
   alignment_pdm <- normalise.matrix(alignment_pdm)
   #print(alignment_pdm) # print scaled pdm matrix
