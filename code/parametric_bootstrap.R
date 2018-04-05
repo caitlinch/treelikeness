9532d159d60c1ee3cb38a5e837b666935084bbb0
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-04-05 14:49:35 +1000

    Function to extract parameters works out whether DNA/amino-acide sequence, extracts that information. Also extracts file name, number of sites, number of taxa and model of substitution used

--- code/parametric_bootstrap.R
+++ code/parametric_bootstrap.R
@@ -59,23 +59,52 @@ do.1.bootstrap <- function(iq_path,folder_path,parameters,test_statistic) {
 
 
 # Given a .iqtree file, this function will extract the relevant parameters
-get.simulation.parameters <- function(folder_path){
-  # Read in the summary information about the alignment
-  alignment_info <- read.table(paste0(folder_path,"/alignment.nex-seq-summary.txt"),header = TRUE)
-  # Extract the number of taxa 
-  ntaxa <- nrow(alignment_info)
-  # Extract the length of the alignment - take maximum length of all input sequences
-  nsites <- max(alignment_info$Sequence_length)
+get.simulation.parameters <- function(dotiqtree_file){
   # read in the IQ-TREE file to get substitution model and parameters
-  iqtree_file <- paste0(folder_path,"/alignment.nex.iqtree")
-  iq_df <- readLines(iqtree_file)
-  # get the start and end of the substitution parameters section in the iqtree file
-  start_ind      <- which(iq_df == "SUBSTITUTION PROCESS")+3
-  end_ind        <- which(iq_df == "MAXIMUM LIKELIHOOD TREE")-2
-  # extract the substitution model lines of the iq_df
-  subs_df <- iq_df[start_ind:end_ind]
-  # parameters in order: number of taxa, number of sites, substitution database
-  pms <- list(ntaxa,nsites,subs_df)
-  return(pms)
+  iq_file <- readLines(dotiqtree_file)
+  # extract the file name
+  ind      <- grep("Input file name:",iq_file)
+  op1      <- substr(iq_file[[ind]],18,nchar(iq_file[[ind]]))
+  # extract the number of taxa and extract the length of the alignment
+  ind         <- grep("Input data:",iq_file)
+  input_str   <- iq_file[[ind]] # get the line that contains this info
+  input_ls    <- strsplit(input_str," ")
+  op2         <- input_ls[[1]][3] # extract number of sequences (number of taxa)
+  op3         <- input_ls[[1]][6] # extract number of sites 
+  # Extract the model of substitution (same for amino-acid and nucleotide files)
+  ind         <- grep("Model of substitution: ",iq_file)
+  sub_str     <- iq_file[[ind]] # get the line that contains this info
+  sub_ls      <- strsplit(sub_str," ")
+  op4         <- sub_ls[[1]][4]
+  
+  # check the type of sites - amino acid or DNA
+  # if the input is DNA (nucleotide sites), gather that information
+  if (input_ls[[1]][7]=="nucleotide"){
+    # Extract the rate parameters
+    # A-C: 1.1021
+    # A-G: 3.5984
+    # A-T: 0.6766
+    # C-G: 1.4170
+    # C-T: 3.5984
+    # G-T: 1.0000
+  
+    
+    # make a list of the rows for the output dataframe
+    names <- c("file_name","n_taxa","n_sites","substitution_model", "A-C_rate","A-G_rate","A-T_rate","C-G_rate","C-T_rate","G-T_rate")
+    # Make a list of the output rows for the output dataframe
+    op <- c(op1,op2,op3,op4)
+  } else if (input_ls[[1]][7]=="amino-acid"){ # alternatively if the data is amino acid sites
+    # do nothing for now whoops haha (add this in after you've done the DNA part)
+    # make a list of the rows for the output dataframe
+    names <- c("file_name","n_taxa","n_sites","substitution_model")
+    # Make a list of the output rows for the output dataframe
+    op <- c(op1,op2,op3,op4)
+  }
+  
+  # Now the information has been collected, create an output dataframe
+  # make the output dataframe
+  op_df <- data.frame(names,op)
+  names(op_df) <- c("parameter","value")
+  return(op_df)
 }
 
