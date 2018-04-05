33406bbd6b309af124aa58aadf169b2f61644317
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-04-05 16:52:44 +1000

    Finished function to extract parameters from .iqtree file for DNA

--- code/parametric_bootstrap.R
+++ code/parametric_bootstrap.R
@@ -81,30 +81,96 @@ get.simulation.parameters <- function(dotiqtree_file){
   # if the input is DNA (nucleotide sites), gather that information
   if (input_ls[[1]][7]=="nucleotide"){
     # Extract the rate parameters
-    # A-C: 1.1021
-    # A-G: 3.5984
-    # A-T: 0.6766
-    # C-G: 1.4170
-    # C-T: 3.5984
-    # G-T: 1.0000
-  
+    rate1 <- as.numeric(strsplit(iq_file[[grep("A-C",iq_file)]],":")[[1]][2]) # A-C rate (same as code above, but combined 4 lines into 1 line)
+    rate2 <- as.numeric(strsplit(iq_file[[grep("A-G",iq_file)]],":")[[1]][2]) # A-G rate
+    rate3 <- as.numeric(strsplit(iq_file[[grep("A-T",iq_file)]],":")[[1]][2]) # A-T rate
+    rate4 <- as.numeric(strsplit(iq_file[[grep("C-G",iq_file)]],":")[[1]][2]) # C-G rate
+    rate5 <- as.numeric(strsplit(iq_file[[grep("C-T",iq_file)]],":")[[1]][2]) # C-T rate
+    rate6 <- as.numeric(strsplit(iq_file[[grep("G-T",iq_file)]],":")[[1]][2]) # G-T rate
+    
+    # Extract the state frequencies
+    sf1 <- as.numeric(strsplit(iq_file[[grep("pi\\(A\\)",iq_file)]],"=")[[1]][2]) # pi(A) - A freq. Remember to double backslash to escape before brackets
+    sf2 <- as.numeric(strsplit(iq_file[[grep("pi\\(C\\)",iq_file)]],"=")[[1]][2]) # pi(C) - C freq
+    sf3 <- as.numeric(strsplit(iq_file[[grep("pi\\(G\\)",iq_file)]],"=")[[1]][2]) # pi(G) - G freq
+    sf4 <- as.numeric(strsplit(iq_file[[grep("pi\\(T\\)",iq_file)]],"=")[[1]][2]) # pi(T) - T freq
+    
+    # Extract model of rate heterogeneity
+    mrh1      <- strsplit(iq_file[[grep("Model of rate heterogeneity:",iq_file)]],":")[[1]][2] # Extract model of rate heterogeneity 
+    mrh2      <- as.numeric(strsplit(iq_file[[(grep("Model of rate heterogeneity:",iq_file)+1)]],":")[[1]][2]) # Line after the "model of rate heterogeneity" varies - extract it regardless of what it is 
+    mrh2_name <- strsplit(iq_file[[(grep("Model of rate heterogeneity:",iq_file)+1)]],":")[[1]][1] # As the line varies, extract the name for the output dataframe
+    mrh2_name <- gsub(" ","_",mrh2_name) # change the name to be easy to parse
     
     # make a list of the rows for the output dataframe
-    names <- c("file_name","n_taxa","n_sites","substitution_model", "A-C_rate","A-G_rate","A-T_rate","C-G_rate","C-T_rate","G-T_rate")
+    names <- c("file_name","n_taxa","n_sites","substitution_model", "A-C_rate","A-G_rate","A-T_rate","C-G_rate","C-T_rate","G-T_rate","A_freq","C_freq",
+               "G_freq","T_freq","model_of_rate_heterogeneity",mrh2_name)
     # Make a list of the output rows for the output dataframe
-    op <- c(op1,op2,op3,op4)
+    op <- c(op1,op2,op3,op4,rate1,rate2,rate3,rate4,rate5,rate6,sf1,sf2,sf3,sf4,mrh1,mrh2)
+    # Create the output dataframe
+    op_df <- data.frame(names,op)
+    # Name the columns
+    names(op_df) <- c("parameter","value")
+    
+    # Create the rate matrix Q
+    Q_start <- grep("Rate matrix Q:",iq_file)+2
+    Q_end   <- Q_start+3
+    # Create the columns
+    c1 <- c("A","C","G","T")
+    c2 <- c()
+    c3 <- c()
+    c4 <- c()
+    c5 <- c()
+    # For each row in the iqtree file rate matrix
+    for (i in Q_start:Q_end){
+      # Split the row
+      row <- strsplit(iq_file[[i]],"   ")
+      # Add the resulting values to the relevant columns
+      c2 <- c(c2,as.numeric(row[[1]][2])) # convert to numeric so can use the numbers more easily later
+      c3 <- c(c3,as.numeric(row[[1]][3]))
+      c4 <- c(c4,as.numeric(row[[1]][4]))
+      c5 <- c(c5,as.numeric(row[[1]][5]))
+    }
+    # Create a dataframe of the rate matrix Q
+    q_df <- data.frame(c1,c2,c3,c4,c5)
+    #Rename the columns
+    names(q_df) <- c("nucleotide","A","C","G","T")
+    
+    #Create the matrix for discrete gamma categories
+    g_start <- grep(" Category",iq_file)+1 # get the index for the first line of the gamma categories matrix
+    empty   <- which(iq_file=="") # get indexes of all empty lines
+    empty   <- empty[empty>g_start] # get empty lines above gamma categories matrix
+    g_end   <- empty[1]-1 # get end index for gamma categories matrix (one less than next empty line)
+    g1 <- c() # initialise columns to store data in
+    g2 <- c()
+    g3 <- c()
+    # Iterate through rows in gamma matrix
+    for (i in g_start:g_end){
+      row <- strsplit(iq_file[[i]],"        ") # split the rows on the long strong of 0's in the middle
+      g1 <- c(g1,as.numeric(row[[1]][1])) # add the values to the columns
+      g2 <- c(g2,as.numeric(row[[1]][2]))
+      g3 <- c(g3,as.numeric(row[[1]][3]))
+    }
+    g_df <- data.frame(g1,g2,g3) # create a dataframe of the information
+    names(g_df) <- c("category","relative_rate","proportion") # name the columns
+    
+    # Create a list of the three dataframes
+    # This will be the output 
+    params <- list(op_df,q_df,g_df)
+    # Name the parameters so they're easy to access once you've outputted the data
+    names(params) <- c("parameters","Q_rate_matrix","gamma_categories")
+    
   } else if (input_ls[[1]][7]=="amino-acid"){ # alternatively if the data is amino acid sites
     # do nothing for now whoops haha (add this in after you've done the DNA part)
     # make a list of the rows for the output dataframe
     names <- c("file_name","n_taxa","n_sites","substitution_model")
     # Make a list of the output rows for the output dataframe
     op <- c(op1,op2,op3,op4)
+    op_df <- data.frame(names,op)
+    names(op_df) <- c("parameter","value")
+    params <- op_df
   }
   
   # Now the information has been collected, create an output dataframe
   # make the output dataframe
-  op_df <- data.frame(names,op)
-  names(op_df) <- c("parameter","value")
-  return(op_df)
+  return(params)
 }
 
