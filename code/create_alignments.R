2e237ccaccc104eaf4f063874b552a294f22db30
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-05-09 16:47:36 +1000

    Created phylogenetic trees. Output DNA for no SPR moves. Working on outputting DNA after SPR move, based on the proportion of each tree

--- code/create_alignments.R
+++ code/create_alignments.R
@@ -2,7 +2,7 @@
 
 # Create a function to make SimBac alignments
 SimBac_make1 <- function(simbac_path, output_folder, ntaxa, nsites, gap, mutation_rate = 0.01, internal_recombination, external_recombination, id = ""){
-  # note - site specific mutation rate defaults to 0.01 when not specified
+  # note - site specific mutation rate defaults to 0.01 when not specified in SimBac
   # Good default gap size is 1,000,000 (1000000)
   output_file <- paste0(output_folder,"SimBac_",ntaxa,"_",nsites,"_",internal_recombination,"_",external_recombination,"_",id,".fasta")
   simbac_command <- paste0(simbac_path," -N ",ntaxa," -B ",nsites," -G ",gap," -T ",mutation_rate," -R ",internal_recombination,
@@ -10,35 +10,43 @@ SimBac_make1 <- function(simbac_path, output_folder, ntaxa, nsites, gap, mutatio
   system(simbac_command)
 }
 
-# Create extreme phylogenetic alignments
-# 1. Simulate a tree
-# simulate a birth-death tree on a fixed number of extant taxa
-# n = number of taxa, numbsim = # of trees to simulate, lambda = speciation rate
-# mu = extinction rate, 
-tree_sim_depth <- sim.bd.taxa(10, numbsim = 1, lambda = 0.5, mu = 0, frac = 1, complete = FALSE, stochsampling = TRUE)[[1]]
-tree_sim_taxa  <- sim.bd.taxa(160, numbsim = 1, lambda = 0.5, mu = 0, frac = 1, complete = FALSE, stochsampling = TRUE)[[1]]
-tree_sim_J     <- sim.bd.taxa(10, numbsim = 1, lambda = 0.5, mu = 0, frac = 1, complete = FALSE, stochsampling = TRUE)[[1]]
-# 2. Simulate rate variation
-# set parameters for creating rate variation in tree
-mol_rate <- 0.1
-mrate_sd <- 0.1
-# make the rate variation in the tree
-rate_var_depth <- tree_sim_depth
-rate_var_depth$edge.length <- tree_sim_depth$edge.length * rlnorm(length(tree_sim_depth$edge.length), meanlog = log(mol_rate), sdlog = mrate_sd)
-rate_var_taxa <- tree_sim_taxa
-rate_var_taxa$edge.length <- tree_sim_taxa$edge.length * rlnorm(length(tree_sim_taxa$edge.length), meanlog = log(mol_rate), sdlog = mrate_sd)
-rate_var_J <- tree_sim_J
-rate_var_J$edge.length <- tree_sim_J$edge.length * rlnorm(length(tree_sim_J$edge.length), meanlog = log(mol_rate), sdlog = mrate_sd)
-# scale tree to have total depth of 0.6 million years (CHECK THIS VALUE & FIND A BIOLOGICAL REASON FOR IT)
-rate_var_depth <- rescale(rate_var_depth,"depth",5)
-rate_var_taxa <- rescale(rate_var_taxa,"depth",0.1)
-rate_var_J <- rescale(rate_var_J,"depth",0.1)
-# 3. Perform an SPR move for the rate_var_J simulation
-rate_var_K <- rSPR(rate_var_J,moves=1)
+# want to feed in a vector of J % that you want to test
+
+# Create a function to make phylogenetic alignments (as outlined in simulation scheme)
+# Provide J_vector in decimals (e.g. 1% = 0.01, 50% = 0.5)
+# J is the proportion of the SECOND tree that will be included
+phylo_make1 <- function(ntaxa, nsites, birth_rate = 0.5, death_rate = 0, tree_age, mol_rate = 0.1, mol_rate_sd = 0.1, J_vector = c()){
+  # 1. Simulate a tree
+  # simulate a birth-death tree on a fixed number of extant taxa
+  # n = number of taxa, numbsim = # of trees to simulate, lambda = speciation rate [good default = 0.5 from Duchenne and Lanfear (2015)]
+  # mu = extinction rate (default for this project = 0)
+  tree_sim <- sim.bd.taxa.age(n = ntaxa, numbsim = 1, lambda = birth_rate, mu = death_rate, frac = 1, age = tree_age, mrca = FALSE)[[1]]
+  tree_sim$edge.length <- tree_sim$edge.length * (tree_age / max(branching.times(tree_sim)))
+  # 2. Simulate rate variation
+  # Default for mol_rate and mol_rate_sd = 0.1 as in Duchenne and Lanfear (2015)
+  phylo_sim <- tree_sim
+  phylo_sim$edge.length <- tree_sim$edge.length * rlnorm(length(tree_sim$edge.length), meanlog = log(mol_rate), sdlog = mol_rate_sd)
+  # scale tree to have a total depth of tree age
+  phylo_sim <- rescale(phylo_sim,"depth",tree_age)
+  # If the J vector is empty, simply simulate DNA along the tree
+  if (length(J_vec)==0){
+    dna_sim <- as.DNAbin(simSeq(phylo_sim),l = nsites)
+  }
+  else {
+    # If there are elements in the J vector, need to create a 2nd alignment to concatenate at those intervals
+    phylo_sim_2 <- rSPR(phylo_sim, moves=1) # perform a single SPR move at random
+    K_vector <- 1 - J_vector # proportion of first tree that will be included
+    dna_sim_1 <- as.DNAbin(simSeq(phylo_sim),l = K_vector*nsites) # simulate along the entire first tree
+    dna_sim_2 <- as.DNAbin(simSeq(phylo_sim_2),l = J_vector*nsites) # simulate along the entire second tree
+    
+  }
+}
+
+
 # 4. Simulate an alignment 
 # simulate the sequence alignment based on the tree and number of sites
 dna_sim_depth <- as.DNAbin(simSeq(rate_var_depth, l = 1300))
-dna_sim_taxa <- as.DNAbin(simSeq(rate_var_taxa, l = 1300))
+
 dna_sim_J <- as.DNAbin(simSeq(rate_var_J, l = 650))
 dna_sim_K <- as.DNAbin(simSeq(rate_var_K, l = 650))
 # concatenate alignments J and K
@@ -50,6 +58,3 @@ op_file <- paste0(test_folder,"/alignment_phylo_taxa.nexus")
 write.nexus.data(dna_sim_taxa, file = op_file)
 op_file <- paste0(test_folder,"/alignment_phylo_5050.nexus")
 write.nexus.data(dna_sim_5050, file = op_file)
-
-# Create a function to make phylogenetic alignments (as outlined in simulation scheme)
-phylo_make1 <- 
