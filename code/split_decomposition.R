533e80d3a2431a568a8da907a484601bb1c360b1
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-05-14 14:53:49 +1000

    Fixed input to SplitsTree so that it outputs a new nexus file that includes split decomposition results

--- code/split_decomposition.R
+++ code/split_decomposition.R
@@ -223,7 +223,8 @@ splits.filename <- function(alignment_path){
 call.SplitsTree <- function(splitstree_path,alignment_path){
   output_path <- splits.filename(alignment_path)
   # Call splitstree and do the split decomposition, save the results (overwrite any existing results)
-  splitstree_command <- paste0(splitstree_path, " -g -x 'OPEN FILE=", alignment_path,"; UPDATE SplitDecomposition; SAVE FILE=", output_path," REPLACE=yes; QUIT'")
+  splitstree_command <- paste0(splitstree_path, " -g -x 'OPEN FILE=", alignment_path,"; ASSUME chartransform =Uncorrected_P HandleAmbiguousStates=Ignore Normalize=true; ASSUME disttransform=SplitDecomposition; SAVE FILE=", output_path," REPLACE=yes; QUIT'")
+  print(splitstree_command)
   system(splitstree_command)
 }
 
