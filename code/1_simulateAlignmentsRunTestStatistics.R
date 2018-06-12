c245affc26d592be7f8d4dc273f22ea61ea4af0d
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-06-12 13:00:01 +1000

    Updated K values to be decimals NOT percentages

--- code/1_simulateAlignmentsRunTestStatistics.R
+++ code/1_simulateAlignmentsRunTestStatistics.R
@@ -30,19 +30,11 @@ names(exec_paths) <- c("3seq","IQTree","Phi","SimBac","SplitsTree")
 ## If running 3seq remotely, need to specify the ptable before the first run so that 3seq knows where to look
 #system("./3seq -f mtDNA.aln -ptable PvalueTable500 -id myFirstRun") # this command associates the Ptable with 3seq - needs a sample alignment to run correctly.
 
-baby_phylo_df <- data.frame(matrix(nrow=0,ncol=10))
-phylo_row <- c("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",20,1000,0.5,1,0.1,0.1,0.5,"testBaby",1)
-baby_phylo_df <- rbind(baby_phylo_df,phylo_row,phylo_row,phylo_row,stringsAsFactors=FALSE)
-phylo_names <- c("output_folder","n_taxa","n_sites","birth_rate","tree_age","mean_molecular_rate","sd_molecular_rate","proportion_tree2","id","rep")
-names(baby_phylo_df) <- phylo_names
-baby_phylo_df[2,2]<- 180
-baby_phylo_df[3,2]<- 6
-baby_simbac_df <- data.frame(matrix(nrow=0,ncol=9))
-simbac_row <- c("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",20,1000,1000000,0.1,0,0.1,"testBaby",1)
-baby_simbac_df <- rbind(baby_simbac_df,simbac_row,simbac_row,stringsAsFactors=FALSE)
-simbac_names <- c("output_folder","n_taxa","n_sites","gap","internal_recombination","external_recombination","mutation_rate","id","rep")
-names(baby_simbac_df) <- simbac_names
+# Columns required for the rows of parameters to run the simulations
+phylo_df_names <- c("output_folder","n_taxa","n_sites","birth_rate","tree_age","mean_molecular_rate","sd_molecular_rate","proportion_tree2","id","rep")
+simbac_df_names <- c("output_folder","n_taxa","n_sites","gap","internal_recombination","external_recombination","mutation_rate","id","rep")
 
+# Build some test dataframes
 output_folder <- rep("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",20)
 n_taxa <- rep(20,20)
 n_sites <- rep(1300,20)
@@ -52,7 +44,7 @@ external_recombination <- rep(c(0.00,0.01,0.05,0.10),5)
 mutation_rate <- rep(0.01,20)
 id <- rep(c("external"),20)
 rep <- c(rep(1,4), rep(2,4),rep(3,4),rep(4,4),rep(5,4))
-external_df <- data.frame(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep)
+external_df <- data.frame(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep, stringsAsFactors = FALSE)
 
 output_folder <- rep("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",20)
 n_taxa <- rep(20,20)
@@ -63,7 +55,7 @@ external_recombination <- rep(c(0.00,0.00,0.00,0.0),5)
 mutation_rate <- rep(0.01,20)
 id <- rep(c("internal"),20)
 rep <- c(rep(1,4), rep(2,4),rep(3,4),rep(4,4),rep(5,4))
-internal_df <- data.frame(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep)
+internal_df <- data.frame(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep, stringsAsFactors = FALSE)
 
 output_folder <- rep("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",20)
 n_taxa <- rep(20,20)
@@ -74,21 +66,35 @@ external_recombination <- rep(c(0.00,0.00,0.00,0.0),5)
 mutation_rate <- rep(0.01,20)
 id <- rep(c("mutation"),20)
 rep <- c(rep(1,4), rep(2,4),rep(3,4),rep(4,4),rep(5,4))
-mutation_df <- data.frame(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep)
+mutation_df <- data.frame(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep, stringsAsFactors = FALSE)
 
-c("output_folder","n_taxa","n_sites","birth_rate","tree_age","mean_molecular_rate","sd_molecular_rate","proportion_tree2","id","rep")
-output_folder <- rep("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",20)
+output_folder <- rep("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",255)
 n_taxa <- rep(20,255)
 n_sites <- rep(1300,255)
 birth_rate <- rep(0.5,255)
 tree_age <- rep(1,255)
 mean_molecular_rate <- rep(0.1,255)
 sd_molecular_rate <- rep(0.1,255)
-proportion_tree2 <- rep(1:51,5)
-id <- rep(c("phylo"),240)
+proportion_tree2 <- rep(seq(0,0.5,0.01),5)
+id <- rep(c("phylo"),255)
 rep <- c(rep(1,51), rep(2,51),rep(3,51),rep(4,51),rep(5,51))
-phylo_df <- data.frame(output_folder,n_taxa,n_sites,birth_rate,tree_age,internal_recombination,external_recombination,proportion_tree2,id,rep)
+phylo_df <- data.frame(output_folder,n_taxa,n_sites,birth_rate,tree_age,mean_molecular_rate,sd_molecular_rate,proportion_tree2,id,rep, stringsAsFactors = FALSE)
 
+output_folder <- rep("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",204)
+n_taxa <- rep(20,204)
+n_sites <- rep(1300,204)
+birth_rate <- rep(0.5,204)
+tree_age <- rep(1,204)
+mean_molecular_rate <- rep(c(0.05,0.1,0.5,1),51)
+sd_molecular_rate <- rep(0.1,204)
+proportion_tree2 <- rep(seq(0,0.5,0.01),4)
+id <- rep(c("phylo"),204)
+rep <- c(rep(1,51), rep(2,51),rep(3,51),rep(4,51))
+depth_df <- data.frame(output_folder,n_taxa,n_sites,birth_rate,tree_age,mean_molecular_rate,sd_molecular_rate,proportion_tree2,id,rep, stringsAsFactors = FALSE)
+
+# Use the apply functions to run the simulations
+#apply(phylo_df,1,phylo.run1sim,exec_paths)
+#phylo.run1sim(phylo_df[1,],exec_paths)
 
 
 
