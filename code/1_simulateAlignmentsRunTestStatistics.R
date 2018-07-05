0d9eb0760dd784eeab91819ba98a2c4c2b213160
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-07-05 10:49:42 +1000

    Added small alignment (6 taxa) to check test statistic results by hand

--- code/1_simulateAlignmentsRunTestStatistics.R
+++ code/1_simulateAlignmentsRunTestStatistics.R
@@ -20,6 +20,7 @@ setwd(maindir)
 source(paste0(maindir,"code/func_split_decomposition.R"))
 source(paste0(maindir,"code/func_test_statistic.R"))
 source(paste0(maindir,"code/func_create_alignments.R"))
+source(paste0(maindir,"code/func_process_data.R"))
 
 # Create a vector with all of the executable file paths
 # To access a path: exec_paths[["name"]]
@@ -96,10 +97,23 @@ tree_age <- rep(1,204)
 mean_molecular_rate <- rep(c(0.05,0.1,0.5,1),51)
 sd_molecular_rate <- rep(0.1,204)
 proportion_tree2 <- rep(seq(0,0.5,0.01),4)
-id <- rep(c("phylo"),204)
+id <- rep(c("molRate"),204)
 rep <- c(rep(1,51), rep(2,51),rep(3,51),rep(4,51))
 depth_df <- data.frame(output_folder,n_taxa,n_sites,birth_rate,tree_age,mean_molecular_rate,sd_molecular_rate,proportion_tree2,id,rep, stringsAsFactors = FALSE)
 lapply(1:nrow(depth_df),phylo.rowWrapper,dataframe = depth_df, program_paths = exec_paths)
+
+output_folder <- rep("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/",1)
+n_taxa <- rep(6,1)
+n_sites <- rep(1300,1)
+birth_rate <- rep(0.5,1)
+tree_age <- rep(1,1)
+mean_molecular_rate <- rep(0.1,1)
+sd_molecular_rate <- rep(0.1,1)
+proportion_tree2 <- rep(0.25,1)
+id <- rep(c("CheckTestStats"),1)
+rep <- rep(1,1)
+check_df <- data.frame(output_folder,n_taxa,n_sites,birth_rate,tree_age,mean_molecular_rate,sd_molecular_rate,proportion_tree2,id,rep, stringsAsFactors = FALSE)
+lapply(1:nrow(check_df),phylo.rowWrapper,dataframe = check_df, program_paths = exec_paths)
 
 
 
