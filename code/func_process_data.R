07f6c9a3b42c15d662c3034732c1117bd15e9863
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-07-05 14:54:35 +1000

    Added functions to remove irrelevant data from the collated dataframe (i.e. things only necessary for other type of simulations such as K% for SimBac sims

--- code/func_process_data.R
+++ code/func_process_data.R
@@ -1,9 +1,5 @@
 # Functions to process and plot test statistics resulting from simulations
 
-directory <- "/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testAlignments2/"
-id <- "CheckTestStats"
-id <- "external"
-
 # Function to collect all test statistic csv folders given an id to look for and a directory to look in
 collate.csv <- function(directory,id){
   # Collect all the folders within the directory
@@ -14,15 +10,28 @@ collate.csv <- function(directory,id){
   csv_paths <- folder_paths[inds]
   # Make the path to each output csv file
   csv_paths <- paste0(directory, csv_paths, "/testStatistics.csv")
-  print(csv_paths)
   # Set the number of rows in the dataframe (will equal the number of folders - one folder per simulation)
   num_rows <- length(csv_paths)
   # Open all the csv files, store the results as a list
-  output_list <- lapply(csv_paths,read.csv)
-  
-  # Create the empty dataframe
-  output_df <- data.frame(matrix(nrow = num_rows, ncol = 32))
-  lapply(1:num_rows, extract.one.csv, paths = csv_paths, df = output_df)
+  output_list <- lapply(csv_paths, read.csv, stringsAsFactors = FALSE)
+  # Reduce the dataframe from a list into a matrix
+  output_df <- Reduce(rbind, output_list)
+  # Remove the "X" column (row number for smaller csv files)
+  output_df <- output_df[,4:33]
   return(output_df)
+}
+
+# Extract the relevant columns for analysing the SimBac simulations
+simplify.Simbac <- function(df){
+  cols <- c(1:5,14:30) 
+  df <- df[cols]
+  return(df)
+}
+
+# Extract the relevant columns for analysing the phylogenetic simulations
+simplify.phylo <- function(df){
+  cols <- c(1:2,6:11,14:30) 
+  df <- df[cols]
+  return(df)
 }
 
