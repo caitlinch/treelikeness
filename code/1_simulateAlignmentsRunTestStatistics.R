d1b78c7e678036f5671f97785c04c412dcc4e702
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-07-13 15:36:07 +1000

    Added file paths for raijin

--- code/1_simulateAlignmentsRunTestStatistics.R
+++ code/1_simulateAlignmentsRunTestStatistics.R
@@ -12,25 +12,38 @@ library(ggplot2)
 library(reshape2)
 library(tictoc) # library for measuring timings! tic("label") to start, toc() to stop
 
+run_location <- "nci"
+run_location <- "mac"
+
+if (run_location == "mac"){
+  op_folder <- "/Users/caitlincherryh/Documents/TestAlignmentResults/1_mainrun/"
+  maindir <- "/Users/caitlincherryh/Documents/Repositories/treelikeness/"
+  exec_folder <- "/Users/caitlincherryh/Documents/Executables/"
+  network_functions <- "code/func_split_decomposition.R"
+} else if (run_location == "nci") {
+  op_folder <- "/short/xf1/cac599/sims_output/"
+  maindir <- "/home/599/cac599/treelikeness/"
+  exec_folder <- "/home/599/cac599/treelikeness/executables/"
+  network_functions <- "code/func_split_decomposition_nci.R"
+}
+
 # Set working directory
-maindir <- "/Users/caitlincherryh/Documents/Repositories/treelikeness/" # for work computer
 setwd(maindir)
 
 # Source files for functions
-source(paste0(maindir,"code/func_split_decomposition.R"))
+source(paste0(maindir,network_functions))
 source(paste0(maindir,"code/func_test_statistic.R"))
 source(paste0(maindir,"code/func_create_alignments.R"))
 source(paste0(maindir,"code/func_process_data.R"))
 
 # Create a vector with all of the executable file paths
 # To access a path: exec_paths[["name"]]
-exec_folder <- "/Users/caitlincherryh/Documents/Executables/"
 exec_paths <- c("3seq","iqtree","Phi","SimBac","SplitsTree.app/Contents/MacOS/JavaApplicationStub")
 exec_paths <- paste0(exec_folder,exec_paths)
 names(exec_paths) <- c("3seq","IQTree","Phi","SimBac","SplitsTree")
 
 ## If running 3seq remotely, need to specify the ptable before the first run so that 3seq knows where to look
-#system("./3seq -f mtDNA.aln -ptable PvalueTable500 -id myFirstRun") # this command associates the Ptable with 3seq - needs a sample alignment to run correctly.
+system(exec_paths[["3seq"]],"-f mtDNA.aln -ptable PvalueTable500 -id myFirstRun") # this command associates the Ptable with 3seq - needs a sample alignment to run correctly.
 
 # Columns required for the rows of parameters to run the simulations
 phylo_df_names <- c("output_folder","n_taxa","n_sites","birth_rate","tree_age","mean_molecular_rate","sd_molecular_rate","proportion_tree2","id","rep")
@@ -41,7 +54,7 @@ simbac_df_names <- c("output_folder","n_taxa","n_sites","gap","internal_recombin
 
 # For external recombination
 # Create dataframe
-output_folder <- c("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/")
+output_folder <- c(op_folder)
 n_taxa <- c(5, 10, 20, 40, 80, 160)
 n_sites <- c(1300)
 gap <- c(1000000)
@@ -53,11 +66,12 @@ rep <- c(1:10)
 external_df <- expand.grid(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep, stringsAsFactors = FALSE)
 names(external_df) <- c("output_folder","n_taxa","n_sites","gap","internal_recombination","external_recombination","mutation_rate","id","rep")
 # run simulations
-lapply(1:nrow(internal_df),SimBac.rowWrapper,dataframe = external_df, program_paths = exec_paths)
+#lapply(1:nrow(internal_df),SimBac.rowWrapper,dataframe = external_df, program_paths = exec_paths)
+lapply(1:2,SimBac.rowWrapper,dataframe = external_df, program_paths = exec_paths)
 
 # For internal recombination
 # Create dataframe
-output_folder <- c("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/")
+output_folder <- c(op_folder)
 n_taxa <- c(5, 10, 20, 40, 80, 160)
 n_sites <- c(1300)
 gap <- c(1000000)
@@ -69,10 +83,10 @@ rep <- c(1:10)
 internal_df <- expand.grid(output_folder,n_taxa,n_sites,gap,internal_recombination,external_recombination,mutation_rate,id,rep, stringsAsFactors = FALSE)
 names(internal_df) <- c("output_folder","n_taxa","n_sites","gap","internal_recombination","external_recombination","mutation_rate","id","rep")
 # run simulations
-lapply(1:nrow(internal_df),SimBac.rowWrapper,dataframe = internal_df, program_paths = exec_paths)
+#lapply(1:nrow(internal_df),SimBac.rowWrapper,dataframe = internal_df, program_paths = exec_paths)
   
 # Create phylogenetic sims dataframe
-output_folder <- c("/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/")
+output_folder <- c(op_folder)
 n_taxa <- c(5, 10, 20, 40, 80, 160)
 n_sites <- c(1300)
 birth_rate <- c(0.5)
@@ -85,7 +99,7 @@ rep <- c(1:10)
 phylo_df <- expand.grid(output_folder,n_taxa,n_sites,birth_rate,tree_age,mean_molecular_rate,sd_molecular_rate,proportion_tree2,id,rep, stringsAsFactors = FALSE)
 names(phylo_df) <- c("output_folder","n_taxa","n_sites","birth_rate","tree_age","mean_molecular_rate","sd_molecular_rate","proportion_tree2","id","rep")
 # Run the simulations
-lapply(1:nrow(phylo_df),phylo.rowWrapper,dataframe = phylo_df, program_paths = exec_paths)
+#lapply(1:nrow(phylo_df),phylo.rowWrapper,dataframe = phylo_df, program_paths = exec_paths)
 
 
 
