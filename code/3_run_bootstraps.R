8046d95d9eb71af2dd279e6578074bb2ba0a8d0b
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-08-07 13:52:06 +1000

    Successful test run for bootstrap

--- code/3_run_bootstraps.R
+++ code/3_run_bootstraps.R
@@ -4,6 +4,7 @@
 source = "mac"
 
 if (source=="mac"){
+  treelikeness_dir <- "/Users/caitlincherryh/Documents/Repositories/treelikeness/"
   test_alignment_folder <- "/Users/caitlincherryh/Documents/Repositories/treelikeness/raw_data/testBootstrap1/Phylo_20_1300_NA_NA_NA_1_0.1_0.47_2trees_5/"
   iq_path <- "/Users/caitlincherryh/Documents/Executables/iqtree"
   splitstree_path <- "/Users/caitlincherryh/Documents/Executables/SplitsTree.app/Contents/MacOS/JavaApplicationStub"
@@ -19,9 +20,9 @@ library(phangorn)
 library(base)
 
 # Source files for functions
-source(paste0(maindir,"code/func_split_decomposition.R"))
-source(paste0(maindir,"code/func_parametric_bootstrap.R"))
-source(paste0(maindir,"code/func_test_statistic.R"))
+source(paste0(treelikeness_dir,"code/func_split_decomposition.R"))
+source(paste0(treelikeness_dir,"code/func_parametric_bootstrap.R"))
+source(paste0(treelikeness_dir,"code/func_test_statistic.R"))
 
 phylo.bootstrap(test_alignment_folder,n_reps,iq_path,splitstree_path)
 
