409a6f4045d0fd5a34ca7ba8974b1d4b59a0958e
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-08-24 18:08:28 +1000

    Renamed plot4_id to id for consistency with runs for other plots

--- code/1_simulateAlignmentsRunTestStatistics.R
+++ code/1_simulateAlignmentsRunTestStatistics.R
@@ -200,7 +200,7 @@ output_folder <- op_folder
 n_sites <- 1300
 tree_age <- c(0.05, 0.1, 0.5, 1)
 proportion_tree2 <- seq(0,0.5,0.1)
-plot4_id <- "plot4"
+id <- "plot4"
 # plot4_id <- "TESTBOOTSTRAP"
 rep <- 1:100
 tree1_vector <- c("08taxa_balanced_LHS","08taxa_balanced_LHS",
@@ -215,14 +215,14 @@ tree_id <- 1:9
 for (i in tree_id){
   tree1_temp <- tree1_vector[[i]]
   tree2_temp <- tree2_vector[[i]]
-  temp_df <- expand.grid(op_folder,n_sites,tree_age,tree1_temp,tree2_temp,proportion_tree2,plot4_id,rep, stringsAsFactors = FALSE)
+  temp_df <- expand.grid(op_folder,n_sites,tree_age,tree1_temp,tree2_temp,proportion_tree2,id,rep, stringsAsFactors = FALSE)
   names(temp_df) <- c("output_folder", "n_sites", "tree_age", "tree1", "tree2", "proportion_tree2", "id", "rep")
   plot4_df <- rbind(plot4_df,temp_df, stringsAsFactors = FALSE)
 }
 mclapply(1:nrow(plot4_df), phylo.fixedtrees.wrapper, plot4_df, exec_paths, tree_folder, mc.cores = 35) # mclapply for phylo with fixed trees
 # Collect the folders that contain the alignments for plot4
 all_folders <- list.dirs(op_folder, recursive = FALSE, full.names = TRUE) # get all the directory names in the output folder
-inds <- grep(plot4_id,all_folders) # find which indexes the plot4 (bootstrap) folders are at 
+inds <- grep(id,all_folders) # find which indexes the plot4 (bootstrap) folders are at 
 plot4_folders <- all_folders[inds] # get the bootstrap folders
 plot4_folders <- paste0(plot4_folders,"/") # add the slash to the end so it's a path to the directory. Bootstrap function just adds "alignment.nex" not the slash.
 mclapply(plot4_folders, phylo.parametric.bootstrap, 199, exec_paths[["IQTree"]], exec_paths[["SplitsTree"]], exec_paths[["Phi"]], exec_paths[["3seq"]], mc.cores = 35) # run all the bootstraps!
