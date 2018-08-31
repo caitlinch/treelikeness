f2c298415e6927c65519b17bea50ed136d0abe09
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-08-31 15:03:54 +1000

    Removed gamma categories - just simulates one alignment (this is ok because model is fixed as JC). Added checks - if alignment exists, won't make new ones. If 3seq/PhiPack have already been run, doesn't rerun. If test statistic results for that replicate exist, doesn't rerun those. Otherwise reruns everything. Now can use on a set of files where some have completed bootstrap replicates and others don't without worrying about parts of the data being overwritten/changed.

--- code/func_parametric_bootstrap.R
+++ code/func_parametric_bootstrap.R
@@ -37,23 +37,31 @@ do.1.bootstrap <- function(rep_number,params,tree,alignment_folder,iq_path,split
   bs_folder <- paste0(alignment_folder,"bootstrap_",rep_number,"/")
   
   if (dir.exists(bs_folder)==TRUE){
-    redo <- TRUE
+    if (file.exists(paste0(bs_folder,"bootstrap_testStatistics.csv")) == TRUE) {
+      redo <- TRUE # if the test statistics file exists, this is a redo and doesn't need doing
+    } else {
+      # The folder exists but doesn't have the bootstrap replicate test statistics - need to run those so make redo = FALSE (will not be redoing)
+      redo = FALSE 
+    }
   } else if (dir.exists(bs_folder)==FALSE){
     # if the folder doesn't exist, create it
     dir.create(bs_folder)
     redo <- FALSE  # if the directory and the alignment don't exist, this is not a redo and the alignment needs to be tested
   } 
   
-  # Only create alignments, run IQ-Tree and all the tests/test statistics and everything if this is NOT a redo (redo == FALSE)
-  if (redo == FALSE){
-    # Change the working directory to the bootstrap folder
-    setwd(bs_folder)
-    
-    # Create the alignment 
+  # Check if the alignment exists
+  alignment.exists <- file.exists(paste0(bs_folder,"alignment.nexus"))
+  
+  # Change the working directory to the bootstrap folder
+  setwd(bs_folder)
+  
+  if (alignment.exists == FALSE) {
+    # If the alignment doesn't exist, create it
+
     # Sample code for generating a parametric DNA sequence if you have a tree
     # s1 = simSeq(t1, l = 500, type="DNA", bf=c(.25,.25,.25,.25), Q=c(1,1,1,1,1,1), rate=1)
     # s2 = simSeq(t2, l = 500, type="DNA", bf=c(.25,.25,.25,.25), Q=c(1,1,1,1,1,1), rate=1)
-    # aln = c(s1, s2)
+    # aln = c(s1, s2) # concatenate the alignments
     
     # Extract the parameters you need to enter into simSeq
     n_bp = as.numeric(params$parameters[4,2]) # want a sequence that is 1300 base pairs long
@@ -65,56 +73,9 @@ do.1.bootstrap <- function(rep_number,params,tree,alignment_folder,iq_path,split
     seq_type <- "DNA" # generate DNA sequence
     
     # Generate the DNA sequence
-    gamma_categories <- params$gamma_categories
-    if (class(gamma_categories) == "character"){
-      # This means that gamma_categories = "Uniform"
-      # No rate heterogeneity exists - generate the DNA all in one chunk
-      dna_sim <- simSeq(tree, l = n_bp, type = seq_type, bf = base_freqs, Q = Q_vec)
-    } else if (class(gamma_categories) == "data.frame") {
-      # If the gamma categories are a dataframe, there's rate heterogeneity happening
-      # If there's rate heterogeneity, stick together a bunch of alignments to make one big alignment
-      gamma_categories$n_bp <- (gamma_categories$proportion * n_bp)
-      
-      # Make sure that the sum of alignment types will be n_bp (1300 bp)
-      tot <- sum(gamma_categories$n_bp)
-      diff <- n_bp - tot
-      if (diff > 0) {
-        # if there are less bp in the gamma categories than the desired number of base pairs
-        # need to add the diff onto one category
-        # Randomly pick one row to add
-        change_row <- sample(1:nrow(gamma_categories),1)
-        # Add the difference to the randomly chosen row
-        gamma_categories$n_bp[change_row] <- gamma_categories$n_bp[change_row] + diff
-      } else if (diff < 0){
-        # if there are more bp in the gamma categories than the desired number of base pairs
-        # need to subtract the diff from one category
-        # Randomly pick one row to subtract
-        change_row <- sample(1:nrow(gamma_categories),1)
-        # Subtract the difference from the randomly chosen row
-        gamma_categories$n_bp[change_row] <- gamma_categories$n_bp[change_row] - diff
-      }
-    # Now you have the number of bp for each rate, you can create the alignments and stick them together
-    for (i in 1:nrow(gamma_categories)){
-      # Iterate through each row of the gamma categories matrix
-      # Extract the rate for that chunk of the sequence
-      chunk_rate <- gamma_categories$relative_rate[[i]]
-      # Extract the number of base pairs for that chunk of the sequence
-      chunk_length <- gamma_categories$n_bp[[i]]
-      # Simulate the DNA
-      dna_chunk <- simSeq(tree, l = chunk_length, type = seq_type, bf = base_freqs, Q = Q_vec, rate = chunk_rate)
-      dnabin_chunk <- as.DNAbin(dna_chunk)
-      # Stick the DNA together with the DNA already created (or the empty object to store DNA in)
-      if (exists("dna_bin") == FALSE){
-        # If the dna_bin object is empty, this is the first chunk. Make it the dna_bin object
-        dna_bin <- dnabin_chunk
-      } else if (exists("dna_bin") == TRUE){
-        # Otherwise, the dna_bin object exists. Concatenate the dna chunk onto the dna_bin object
-        dna_bin <- cbind(dna_bin,dnabin_chunk, check.names = TRUE, fill.with.gaps = TRUE, quiet = FALSE) # concatenate the two alignments
-      }
-    }
-    dna_sim <- as.phyDat(dna_bin) # convert the alignment to  phydat format
-    }
-      
+    # Don't need to specify rate variation because using JC model with no rate options
+    dna_sim <- simSeq(tree, l = n_bp, type = seq_type, bf = base_freqs, Q = Q_vec)
+    
     # Save the DNA alignment
     bs_al <- paste0(bs_folder, "alignment.nexus")
     write.phyDat(dna_sim,file = bs_al, format = "nexus",interleaved = TRUE, datablock = FALSE) # write the output as a nexus file
@@ -123,116 +84,120 @@ do.1.bootstrap <- function(rep_number,params,tree,alignment_folder,iq_path,split
     ind <- grep("BEGIN CHARACTERS",nexus)+2 # find which line
     nexus[ind] <- "  FORMAT DATATYPE=DNA MISSING=? GAP=- INTERLEAVE;" # replace the line
     writeLines(nexus,bs_al) # output the edited nexus file
-    
+  }
+  
+  # Only create alignments, run IQ-Tree and all the tests/test statistics and everything if this is NOT a redo (redo == FALSE)
+  if (redo == FALSE){
     # The alignment now definitely exists. Now you can run IQ-tree on the alignment
     n_taxa <- as.numeric(params$parameters[3,2]) # extract the number of taxa from the parameters 
     call.IQTREE.quartet.bootstrap(iq_path,bs_al,n_taxa)
-  }
-  bs_al <- paste0(bs_folder, "alignment.nexus") # alignment name
-  
-  ## Calculate the test statistics
-  # run PHIPACK and 3seq
-  phi_path <- phipack_path # get path to phipack executable
-  seq_path <- threeseq_path # get path to 3seq executable
-  filetype = tail(strsplit(bs_al,"\\.")[[1]],n=1) # extract file format
-  if (filetype == "fasta"){
-    # if the alignment is already in fasta format, run PhiPack through R
-    phi_command <- paste0(phi_path," -f ",bs_al, " -v") # assemble system command
-    system(phi_command) #call phipack
     
-    seq_command <- paste0(seq_path," -f ", bs_al)
-    system(seq_command) #call 3SEQ
-  } else if (filetype == "nexus"){
-    # Phipack only reads in Phylip or fasta format - need to convert if the alignment is a nexus file
-    data = read.nexus.data(bs_al) # read in nexus format alignment
-    fasta.name <- paste0(bs_al,".fasta") # make a name for the fasta alignment by adding .fasta (super original ;) )
-    write.fasta(sequences = data,names = names(data), file.out = fasta.name) # output alignment as a fasta format
-    phi_command <- paste0(phi_path," -f ",fasta.name, " -v") # assemble system command as above
-    system(phi_command) # run PHI test on the new fasta alignment
+    ## Calculate the test statistics
+    # check is file is nexus or fasta
+    filetype = tail(strsplit(bs_al,"\\.")[[1]],n=1) # extract file format
+    if (filetype == "nexus"){
+      # Phipack only reads in Phylip or fasta format - need to convert if the alignment is a nexus file
+      data = read.nexus.data(bs_al) # read in nexus format alignment
+      fasta.name <- paste0(bs_al,".fasta") # make a name for the fasta alignment by adding .fasta (super original ;) )
+      write.fasta(sequences = data,names = names(data), file.out = fasta.name) # output alignment as a fasta format
+      al_call_name <- fasta.name
+    } else if (filetype == "fasta"){
+      al_call_name <- bs_al
+    }
     
-    seq_command <- paste0(seq_path," -f ", fasta.name)
-    system(seq_command) #call 3SEQ
+    # run PHIPACK and 3seq (only if you haven't already - test by checking if the log files exist)
+    phi_path <- phipack_path # get path to phipack executable
+    seq_path <- threeseq_path # get path to 3seq executable
+    
+    if (file.exists(paste0(bs_folder,"3s.log")) == FALSE){
+      seq_command <- paste0(seq_path," -f ", al_call_name)
+      system(seq_command) #call 3SEQ
+    }
+    if (file.exists(paste0(bs_folder,"Phi.log")) == FALSE){
+      phi_command <- paste0(phi_path," -f ",al_call_name, " -v") # assemble system command
+      system(phi_command) #call phipack
+    }
+    
+    # Extract significance from Phi Pack output
+    phi_file <- paste0(bs_folder,"Phi.log")
+    phi_file <- readLines(phi_file)
+    ind      <- grep("p-Value",phi_file)
+    phi_sig <- as.numeric(strsplit(phi_file[ind+3],":")[[1]][2])
+    ind      <- grep("PHI Values",phi_file)
+    phi_mean <- as.numeric(strsplit(phi_file[ind+4],"      ","")[[1]][2])
+    phi_var <- as.numeric(strsplit(phi_file[ind+5],"      ","")[[1]][2])
+    phi_obs <- as.numeric(strsplit(phi_file[ind+6],"      ","")[[1]][2])
+    
+    # Extract results output from 3Seq output
+    seq_file <- paste0(bs_folder,"3s.log")
+    seq_log <- readLines(seq_file) # open file
+    ind      <- grep("Number of recombinant triplets",seq_log) # find the number of recombinant triplets line index
+    num_trips <- seq_log[ind]
+    num_trips <- strsplit(num_trips,":")[[1]][2] # extract the number of recombinant triplets
+    num_trips <- trimws(num_trips) # trim the whitespace from the number of triplets
+    ind      <- grep("Number of distinct recombinant sequences",seq_log) # find the number of distinct recombinant sequences line index
+    num_dis <- seq_log[ind]
+    num_dis <- strsplit(num_dis,":")[[1]][2] # extract the number of distinct recombinant sequences
+    num_dis <- trimws(num_dis) # trim the whitespace from the number of distinct recombinant sequences
+    # null hypothesis is of clonal evolution - need significant p-value to accept the alternative hypothesis
+    ind      <- grep("Rejection of the null hypothesis of clonal evolution",seq_log) # find the p value line index
+    seq_sig <- seq_log[ind]
+    seq_sig <- strsplit(seq_sig,"=")[[1]][2] # extract the p value
+    seq_sig <- trimws(seq_sig) # trim the whitespace from the number of distinct recombinant sequences
+    
+    # Extract quartet mapping from IQ-Tree results
+    iq_log_path <- paste0(bs_al, ".iqtree")
+    iq_log <- readLines(iq_log_path)
+    ind <- grep("Number of fully resolved  quartets",iq_log)
+    resolved_q <- as.numeric(strsplit(strsplit(iq_log[ind],":")[[1]][2],"\\(")[[1]][1])
+    ind <- grep("Number of partly resolved quartets",iq_log)
+    partly_resolved_q <- as.numeric(strsplit(strsplit(iq_log[ind],":")[[1]][2],"\\(")[[1]][1])
+    ind <- grep("Number of unresolved",iq_log)
+    unresolved_q <- as.numeric(strsplit(strsplit(iq_log[ind],":")[[1]][2],"\\(")[[1]][1])
+    total_q <- (resolved_q+partly_resolved_q+unresolved_q)
+    prop_resolved <- resolved_q/total_q
+    
+    # Run my test statistics
+    # Want the path to be the path to the alignment you're testing - here that's the bootstrap alignment (bs_al)
+    # run pdm ratio (TS1) (modified splittable percentage)
+    splittable_percentage <- pdm.ratio(iqpath = iq_path, path = bs_al)
+    # run normalised.pdm.difference.sum (TS2a) (sum of difference of normalised matrix)
+    npds <- normalised.pdm.diff.sum(iqpath = iq_path, path = bs_al)
+    # run normalised pdm difference average (TS2b) (mean of difference of normalised matrix)
+    npdm <- normalised.pdm.diff.mean(iqpath = iq_path, path = bs_al)
+    # run split decomposition (TS3) (split decomposition using splitstree)
+    sd <- SplitsTree.decomposition.statistic(iqpath = iq_path, splitstree_path = splitstree_path, path = bs_al, network_algorithm = "split decomposition")
+    # run NeighbourNet (TS3, with neighbour net not split decomposition using splitstree)
+    nn <- SplitsTree.decomposition.statistic(iqpath = iq_path, splitstree_path = splitstree_path, path = bs_al, network_algorithm = "neighbournet")
+    
+    # Extract params csv from alignment folder
+    all_files <- list.files(alignment_folder) # get a list of all the files
+    ind <- grep("params",all_files) # find which of those files is the parameters file
+    params_csv <- read.csv(paste0(alignment_folder,all_files[ind])) # open the parameter file
+    params_csv <- params_csv[,2:ncol(params_csv)]
+    
+    # add test statistic results to params_csv
+    params_csv$PHI_mean <- phi_mean
+    params_csv$PHI_variance <- phi_var
+    params_csv$PHI_observed <- phi_obs
+    params_csv$X3SEQ_num_recombinant_triplets <- num_trips
+    params_csv$X3SEQ_num_distinct_recombinant_sequences <- num_dis
+    params_csv$num_quartets <- total_q
+    params_csv$num_resolved_quartets <- resolved_q
+    params_csv$prop_resolved_quartets <- prop_resolved
+    params_csv$num_partially_resolved_quartets <- partly_resolved_q
+    params_csv$num_unresolved_quartets <- unresolved_q
+    params_csv$splittable_percentage <- splittable_percentage
+    params_csv$pdm_difference <- npds
+    params_csv$pdm_average <- npdm
+    params_csv$split_decomposition <- sd
+    params_csv$neighbour_net <- nn
+    params_csv$bootstrap_id <- paste0("bootstrap_",rep_number)
+    
+    # Output results as a csv in the bootstrap folder
+    bs_results_csv <- paste0(bs_folder,"bootstrap_testStatistics.csv")
+    write.csv(params_csv,file = bs_results_csv)
   }
-  # Extract significance from Phi Pack output
-  phi_file <- paste0(bs_folder,"Phi.log")
-  phi_file <- readLines(phi_file)
-  ind      <- grep("p-Value",phi_file)
-  phi_sig <- as.numeric(strsplit(phi_file[ind+3],":")[[1]][2])
-  ind      <- grep("PHI Values",phi_file)
-  phi_mean <- as.numeric(strsplit(phi_file[ind+4],"      ","")[[1]][2])
-  phi_var <- as.numeric(strsplit(phi_file[ind+5],"      ","")[[1]][2])
-  phi_obs <- as.numeric(strsplit(phi_file[ind+6],"      ","")[[1]][2])
-  
-  # Extract results output from 3Seq output
-  seq_file <- paste0(bs_folder,"3s.log")
-  seq_log <- readLines(seq_file) # open file
-  ind      <- grep("Number of recombinant triplets",seq_log) # find the number of recombinant triplets line index
-  num_trips <- seq_log[ind]
-  num_trips <- strsplit(num_trips,":")[[1]][2] # extract the number of recombinant triplets
-  num_trips <- trimws(num_trips) # trim the whitespace from the number of triplets
-  ind      <- grep("Number of distinct recombinant sequences",seq_log) # find the number of distinct recombinant sequences line index
-  num_dis <- seq_log[ind]
-  num_dis <- strsplit(num_dis,":")[[1]][2] # extract the number of distinct recombinant sequences
-  num_dis <- trimws(num_dis) # trim the whitespace from the number of distinct recombinant sequences
-  # null hypothesis is of clonal evolution - need significant p-value to accept the alternative hypothesis
-  ind      <- grep("Rejection of the null hypothesis of clonal evolution",seq_log) # find the p value line index
-  seq_sig <- seq_log[ind]
-  seq_sig <- strsplit(seq_sig,"=")[[1]][2] # extract the p value
-  seq_sig <- trimws(seq_sig) # trim the whitespace from the number of distinct recombinant sequences
-  
-  
-  # Extract quartet mapping from IQ-Tree results
-  iq_log_path <- paste0(bs_al, ".iqtree")
-  iq_log <- readLines(iq_log_path)
-  ind <- grep("Number of fully resolved  quartets",iq_log)
-  resolved_q <- as.numeric(strsplit(strsplit(iq_log[ind],":")[[1]][2],"\\(")[[1]][1])
-  ind <- grep("Number of partly resolved quartets",iq_log)
-  partly_resolved_q <- as.numeric(strsplit(strsplit(iq_log[ind],":")[[1]][2],"\\(")[[1]][1])
-  ind <- grep("Number of unresolved",iq_log)
-  unresolved_q <- as.numeric(strsplit(strsplit(iq_log[ind],":")[[1]][2],"\\(")[[1]][1])
-  total_q <- (resolved_q+partly_resolved_q+unresolved_q)
-  prop_resolved <- resolved_q/total_q
-  
-  # Run my test statistics
-  # Want the path to be the path to the alignment you're testing - here that's the bootstrap alignment (bs_al)
-  # run pdm ratio (TS1) (modified splittable percentage)
-  splittable_percentage <- pdm.ratio(iqpath = iq_path, path = bs_al)
-  # run normalised.pdm.difference.sum (TS2a) (sum of difference of normalised matrix)
-  npds <- normalised.pdm.diff.sum(iqpath = iq_path, path = bs_al)
-  # run normalised pdm difference average (TS2b) (mean of difference of normalised matrix)
-  npdm <- normalised.pdm.diff.mean(iqpath = iq_path, path = bs_al)
-  # run split decomposition (TS3) (split decomposition using splitstree)
-  sd <- SplitsTree.decomposition.statistic(iqpath = iq_path, splitstree_path = splitstree_path, path = bs_al, network_algorithm = "split decomposition")
-  # run NeighbourNet (TS3, with neighbour net not split decomposition using splitstree)
-  nn <- SplitsTree.decomposition.statistic(iqpath = iq_path, splitstree_path = splitstree_path, path = bs_al, network_algorithm = "neighbournet")
-  
-  # Extract params csv from alignment folder
-  all_files <- list.files(alignment_folder) # get a list of all the files
-  ind <- grep("params",all_files) # find which of those files is the parameters file
-  params_csv <- read.csv(paste0(alignment_folder,all_files[ind])) # open the parameter file
-  params_csv <- params_csv[,2:ncol(params_csv)]
-  # add test statistic results to params_csv
-  
-  params_csv$PHI_mean <- phi_mean
-  params_csv$PHI_variance <- phi_var
-  params_csv$PHI_observed <- phi_obs
-  params_csv$X3SEQ_num_recombinant_triplets <- num_trips
-  params_csv$X3SEQ_num_distinct_recombinant_sequences <- num_dis
-  params_csv$num_quartets <- total_q
-  params_csv$num_resolved_quartets <- resolved_q
-  params_csv$prop_resolved_quartets <- prop_resolved
-  params_csv$num_partially_resolved_quartets <- partly_resolved_q
-  params_csv$num_unresolved_quartets <- unresolved_q
-  params_csv$splittable_percentage <- splittable_percentage
-  params_csv$pdm_difference <- npds
-  params_csv$pdm_average <- npdm
-  params_csv$split_decomposition <- sd
-  params_csv$neighbour_net <- nn
-  params_csv$bootstrap_id <- paste0("bootstrap_",rep_number)
-  
-  # Output results as a csv in the bootstrap folder
-  bs_results_csv <- paste0(bs_folder,"bootstrap_testStatistics.csv")
-  write.csv(params_csv,file = bs_results_csv)
 }
 
 
@@ -473,7 +438,7 @@ phylo.collate.bootstrap <- function(alignment_folder){
                         "pdm_average_p_value","split_decomposition_p_value","neighbour_net_p_value")
   p_value_csv <- paste0(alignment_folder,"p_value.csv")
   write.csv(output_df,file = p_value_csv)
-}
+  }
 
 # Given two vectors (one of test statistic values, and one of ids), calculates the p-value for that alignment
 calculate.p_value <- function(value_vector,id_vector){
