645e5c4768022ff9daab81bfd42174853ba2d78f
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-09-11 17:33:33 +1000

    Preliminary plots of final data

--- code/3_PlotData.R
+++ code/3_PlotData.R
@@ -29,4 +29,18 @@ plot4_df <- read.csv(paste0(input_folder,"plot4_testStatistics_collatedSimulatio
 bs_df <-  read.csv(paste0(input_folder,"plot4_p_value_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
 
 
+# Plot 1:
+ggplot(data=plot1_df,aes(x=tree2_event_position,y=value)) + geom_boxplot(data=plot1_df,aes(fill = tree2_event_type)) + facet_wrap(~variable) + ylim(0,2)
 
+# Plot 2
+ggplot(data=plot2_df,aes(x=proportion_tree2,y=value)) + geom_point(data=plot2_df, aes(colour = as.factor(tree_age))) + facet_wrap(~variable) + geom_smooth(method = "lm", data = plot2_df, aes(x=proportion_tree2,y=value, colour = as.factor(tree_age)))
+ggplot(data=plot2_df,aes(x=proportion_tree2,y=value)) + facet_wrap(~variable) + geom_smooth(method = "lm", data = plot2_df, aes(x=proportion_tree2,y=value, colour = as.factor(tree_age)))
+
+# Plot 3
+plot3_df["event_asfactor"] <- as.factor(plot3_df$number_of_events)
+ggplot(data = plot3_df, aes(x = event_asfactor, y = value)) + geom_boxplot(data = plot3_df, aes(fill = as.factor(tree_age))) + facet_wrap(~variable,scales = "free")
+
+# Plot 4
+ggplot(data=bs_df, aes(x="" ,y = value, fill = as.factor(tree_age))) + geom_boxplot(data = bs_df, aes(fill = as.factor(tree_age))) + facet_grid(as.factor(proportion_tree2)~variable)
+
+       
