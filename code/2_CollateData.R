dc9cdbdbc9cb3bf5265326c6ca6f4b9fc56b7bce
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-09-11 15:15:36 +1000

    Collates simulation output, calculates proportion of recombinant triplets using 3Seq output, and melts data into long format

--- code/2_CollateData.R
+++ code/2_CollateData.R
@@ -1,8 +1,8 @@
 # R code to import and collate test statistic results, and to process the results
 
 # Specify which file paths to use
-# run_location = "mac"
-run_location = "soma"
+run_location = "mac"
+# run_location = "soma"
 
 if (run_location == "mac"){
   # Set file paths etc
@@ -38,20 +38,46 @@ for (i in plot4_ids){
   collate.csv(directory = raw_data_folder, file.name = "p_value", id = i, output_path = output_folder)
 }
 
+# Collate the files for plot4
+csvs <- list.files(output_folder)
+csvs <- csvs[grep("plot4tree",csvs)]
+ts <- csvs[grep("testStatistics_collatedSimulationData",csvs)]
+ts <- paste0(output_folder,ts)
+ts_op_list <- lapply(ts, read.csv, stringsAsFactors = FALSE)
+ts_op_df <- Reduce(rbind, ts_op_list)
+ts_op_df <- ts_op_df[,2:ncol(ts_op_df)]
+write.csv(ts_op_df,file=paste0(output_folder,"plot4_testStatistics_collatedSimulationData.csv"))
+ps <- csvs[grep("p_value_collatedSimulationData",csvs)]
+ps <- paste0(output_folder,ps)
+ps_op_list <- lapply(ps, read.csv, stringsAsFactors = FALSE)
+ps_op_df <- Reduce(rbind, ps_op_list)
+ps_op_df <- ps_op_df[,2:ncol(ps_op_df)]
+write.csv(ps_op_df,file=paste0(output_folder,"plot4_p_value_collatedSimulationData.csv"))
 
+# Calculate the proportion of recombinant triplets and add it onto each set of simulations
+id <- c("plot1_","plot2_","plot3_","plot4_")
+csvs <- list.files(output_folder)
+inds <- lapply(id,grep,csvs)
+csvs <- csvs[unlist(inds)]
+csvs <- csvs[grep("testStatistics_collatedSimulationData",csvs)]
+csvs <- paste0(output_folder,csvs)
+for (csv in csvs){
+  df <- read.csv(csv, stringsAsFactors = FALSE)
+  # divide the number of recombinant triplets detected by 3seq by the number of triplets tested
+  # To find # of triplets: "In a set of 10 sequences, there are 720 unique parent–parent–child arrangements" - Boni et al (2007)
+  # In other words: 6*choose(10,3) == 720
+  # number of triplets tested will be 6* n choose k (if have a,b,c: a and b can be parents, b and c can be parents and a and c can be parents BUT each parent can be either P or Q)
+  df["num_3seq_triplets"] <- 6 * choose(df$n_taxa, 3)
+  df["proportion_recombinant_triplets"] <- df$X3SEQ_num_recombinant_triplets / df$num_3seq_triplets
+  write.csv(df, file = csv)
+}
 
-
-# divide the number of recombinant triplets detected by 3seq by the number of triplets tested
-# number of triplets tested will be 6* n choose k (if have a,b,c: a and b can be parents, b and c can be parents and a and c can be parents BUT each parent can be either P or Q)
-# phylo_df["num_3seq_triplets"] <- 6 * choose(phylo_df$n_taxa, 3)
-# phylo_df["proportion_recombinant_triplets"] <- phylo_df$X3SEQ_num_recombinant_triplets / phylo_df$num_3seq_triplets
-# phylo_df["proportion_tree2"] <- 1 - phylo_df$proportion_tree1 # get the proportion of the second tree (increasing proportion of recombination)
- 
-# Get only the relevant columns to plot
-# PHI (pairwise homoplasy [convergence] index) - gives p-value of observing the sequences under the null hypothesis of no recombination calculated using the PHI statistic.
-# Extract the observed PHI column and the other columns you want (including the new "proportion_recombinant_triplets" column)
-#external_pruned_df <- external_df[ , c("external_recombination","PHI_observed","proportion_recombinant_triplets","prop_resolved_quartets","splittable_percentage","pdm_difference","pdm_average","split_decomposition","neighbour_net")]
-
-
-# # Reshape the data into long format
-# plot_ex_df <- melt(external_pruned_df, id = c("external_recombination"))
+# Reshape the data into long format
+for (csv in csvs){
+  df <- read.csv(csv, stringsAsFactors = FALSE)
+  id_vars <- c("method","n_taxa","n_sites","tree_age","tree1","proportion_tree1","tree2","proportion_tree2","id")
+  measure_vars <- c("PHI_observed","prop_resolved_quartets","proportion_recombinant_triplets","splittable_percentage","pdm_difference","neighbour_net")
+  melt_df <- melt(df, id = id_vars, measure.vars = measure_vars)
+  output_name <- gsub(".csv","_melted.csv",csv)
+  write.csv(melt_df, file = output_name)
+}
