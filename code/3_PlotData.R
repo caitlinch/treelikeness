7595284db0dafab80b29523fb2c47f65ed405f61
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-09-11 15:30:33 +1000

    Open data

--- code/3_PlotData.R
+++ code/3_PlotData.R
@@ -11,17 +11,22 @@ if (run_location == "mac"){
   maindir <- "/Users/caitlincherryh/Documents/Repositories/treelikeness/" # for work computer
 } else if (run_location == "soma") {
   # Set file paths etc
-  raw_data_folder <- "/data/caitlin/treelikeness/output/"
-  output_folder <- "/data/caitlin/treelikeness/results/"
+  input_folder <- "/data/caitlin/treelikeness/results/"
   
   # Set working directory
   maindir <- "/data/caitlin/treelikeness/"
 }
 
-# Source files for functions
-source(paste0(maindir,"code/func_process_data.R"))
-
 # load required libraries
 library(ggplot2)
-library(reshape2)
 library(gridExtra)
+
+# Open dataframes
+plot1_df <- read.csv(paste0(input_folder,"plot1_testStatistics_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
+plot2_df <- read.csv(paste0(input_folder,"plot2_testStatistics_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
+plot3_df <- read.csv(paste0(input_folder,"plot3_testStatistics_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
+plot4_df <- read.csv(paste0(input_folder,"plot4_testStatistics_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
+bs_df <-  read.csv(paste0(input_folder,"plot4_p_value_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
+
+
+
