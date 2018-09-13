eabf179ebf623fd44b678a40d15f29facc3f0d56
caitlinch <24237077+caitlinch@users.noreply.github.com>
2018-09-13 12:31:15 +1000

    Fixed legends and lots of formatting

--- code/3_PlotData.R
+++ code/3_PlotData.R
@@ -7,6 +7,7 @@ run_location = "mac"
 if (run_location == "mac"){
   # Set file paths etc
   input_folder <- "/Users/caitlincherryh/Documents/Results/collatedOutput/"
+  output_folder <- "/Users/caitlincherryh/Documents/Results/plots/seminarPlots/ggsave/"
   # Set working directory
   maindir <- "/Users/caitlincherryh/Documents/Repositories/treelikeness/" # for work computer
 } else if (run_location == "soma") {
@@ -29,121 +30,20 @@ plot3_df <- read.csv(paste0(input_folder,"plot3_testStatistics_collatedSimulatio
 plot4_df <- read.csv(paste0(input_folder,"plot4_testStatistics_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
 bs_df <-  read.csv(paste0(input_folder,"plot4_p_value_collatedSimulationData_melted.csv"), stringsAsFactors = FALSE)
 
-## Exploratory plots
-# Plot 1:
-ggplot(data=plot1_df,aes(x=tree2_event_position,y=value)) + geom_boxplot(data=plot1_df,aes(fill = tree2_event_type)) + facet_wrap(~variable) + ylim(0,2)
-
-# Rob plot 1
-#e = subset(d, tree_age == 0.1) # = df
-e = subset(plot1_df, tree1_tree_shape == 'balanced')
-e = subset(e, tree2_tree_shape == 'balanced')
-e$type = paste(e$tree2_event_type, e$tree2_event_position)
-ggplot(e, aes(x = type, y = value)) +
-  geom_boxplot() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-# Plot 2
-ggplot(data=plot2_df,aes(x=proportion_tree2,y=value)) + geom_point(data=plot2_df, aes(colour = as.factor(tree_age))) + facet_wrap(~variable) + geom_smooth(method = "lm", data = plot2_df, aes(x=proportion_tree2,y=value, colour = as.factor(tree_age)))
-ggplot(data=plot2_df,aes(x=proportion_tree2,y=value)) + facet_wrap(~variable) + geom_smooth(method = "lm", data = plot2_df, aes(x=proportion_tree2,y=value, colour = as.factor(tree_age)))
-# Plot 2 attempt 2
-# reciprocal balanced
-df <- subset(plot2_df, tree1_tree_shape == 'balanced')
-df <- subset(df, tree2_tree_shape == 'balanced')
-df <- subset(df, tree2_event_type == 'reciprocal')
-df <- subset(df, tree2_event_position == 'close')
-df$type = paste(df$tree2_event_type, df$tree2_event_position)
-ggplot(df, aes(x = proportion_tree2, y = value)) +
-  geom_point() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-# non reciprocal balanced
-df <- subset(plot2_df, tree1_tree_shape == 'balanced')
-df <- subset(df, tree2_tree_shape == 'balanced')
-df <- subset(df, tree2_event_type == 'nonreciprocal')
-df <- subset(df, tree2_event_position == 'close')
-df$type = paste(df$tree2_event_type, df$tree2_event_position)
-ggplot(df, aes(x = proportion_tree2, y = value)) +
-  geom_point() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-
-# Plot 3
-plot3_df["event_asfactor"] <- as.factor(plot3_df$number_of_events)
-ggplot(data = plot3_df, aes(x = event_asfactor, y = value)) + geom_boxplot(data = plot3_df, aes(fill = as.factor(tree_age))) + facet_wrap(~variable,scales = "free")
-# attempt 2, reciprocal
-df <- subset(plot3_df, tree1_tree_shape == 'balanced')
-df <- subset(df, tree2_tree_shape == 'balanced')
-df <- subset(df, tree2_event_type == 'reciprocal')
-df <- subset(df, tree2_event_position == 'close')
-df$type = paste(df$tree2_event_type, df$tree2_event_position)
-ggplot(df, aes(x = event_asfactor, y = value)) +
-  geom_boxplot() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-# attempt two, nonreciprocal
-df <- subset(plot3_df, tree1_tree_shape == 'balanced')
-df <- subset(df, tree2_tree_shape == 'balanced')
-df <- subset(df, tree2_event_type == 'nonreciprocal')
-df <- subset(df, tree2_event_position == 'close')
-df$type = paste(df$tree2_event_type, df$tree2_event_position)
-ggplot(df, aes(x = event_asfactor, y = value)) +
-  geom_boxplot() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
-
-# Plot 4
-ggplot(data=bs_df, aes(x="" ,y = value, fill = as.factor(tree_age))) + geom_boxplot(data = bs_df, aes(fill = as.factor(tree_age))) + facet_grid(as.factor(proportion_tree2)~variable)
-ggplot(data=bs_df, aes(x=value)) + geom_histogram() + facet_grid(as.factor(proportion_tree2)~variable)
-# attempt 2, reciprocal
-df <- subset(bs_df, tree1_tree_shape == 'balanced')
-df <- subset(df, tree2_tree_shape == 'balanced')
-df <- subset(df, tree2_event_type == 'reciprocal')
-df <- subset(df, tree2_event_position == 'close')
-df$type = paste(df$tree2_event_type, df$tree2_event_position)
-ggplot(df, aes(x = as.factor(proportion_tree2), y = value)) +
-  geom_boxplot() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))  
-ggplot(df, aes(x = value)) +
-  geom_histogram() +
-  facet_grid(variable~as.factor(proportion_tree2), scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))  
-# attempt 2, nonreciprocal
-df <- subset(bs_df, tree1_tree_shape == 'balanced')
-df <- subset(df, tree2_tree_shape == 'balanced')
-df <- subset(df, tree2_event_type == 'nonreciprocal')
-df <- subset(df, tree2_event_position == 'close')
-df$type = paste(df$tree2_event_type, df$tree2_event_position)
-ggplot(df, aes(x = as.factor(proportion_tree2), y = value)) +
-  geom_boxplot() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
-# histogram?!?!?!?!!
-ggplot(df, aes(x = value)) +
-  geom_histogram() +
-  facet_grid(variable~as.factor(proportion_tree2), scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))    
-
-# no event plots
-df <- subset(bs_df, tree1_tree_shape == 'balanced')
-df <- subset(df, tree2_tree_shape == 'balanced')
-df <- subset(df, tree2_event_type == 'none')
-df <- subset(df, tree2_event_position == 'none')
-df$type = paste(df$tree2_event_type, df$tree2_event_position)
-ggplot(df, aes(x = as.factor(proportion_tree2), y = value)) +
-  geom_boxplot() +
-  facet_grid(variable~tree_age, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) 
-# histogram?!?!?!?!!
-ggplot(df, aes(x = value)) +
-  geom_histogram() +
-  facet_grid(variable~as.factor(proportion_tree2), scales = "free_y") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+# Code for a simple exploratory plot - grids test statistic and tree age, each small plot is event type against test statistic value
+# d = read.csv("~/Dropbox/Projects_Current/tree_likeness/results/plot1_testStatistics_collatedSimulationData_melted.csv")
+# #e = subset(d, tree_age == 0.1)
+# e = subset(d, tree1_tree_shape == 'balanced')
+# e = subset(e, tree2_tree_shape == 'balanced')
+# e$type = paste(e$tree2_event_type, e$tree2_event_position)
+# ggplot(e, aes(x = type, y = value)) +
+#   geom_boxplot() +
+#   facet_grid(variable~tree_age, scales = "free_y") +
+#   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 ## Plots for thesis/seminar
 # Plot 1: How do different events impact detection of recombination?
+print("Plot 1")
 e = subset(plot1_df, tree1_tree_shape == 'balanced')
 e = subset(e, tree2_tree_shape == 'balanced')
 e = subset(e, tree_age == 1 )
@@ -154,7 +54,7 @@ facet_names <- list("neighbour_net" = "Tree-splits proportion","pdm_difference" 
 facet_labeller <- function(variable){
   variable <- facet_names[variable]
 }
-ggplot(e, aes(x = type, y = value)) +
+p <- ggplot(e, aes(x = type, y = value)) +
   geom_boxplot() +
   facet_wrap(~group,scales = "free_y", labeller = labeller(group = facet_labeller)) +
   scale_x_discrete(name = "Type of introgression event",
@@ -165,8 +65,11 @@ ggplot(e, aes(x = type, y = value)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 30), axis.title.x = element_text(size = 40), axis.title.y = element_text(size = 40),
         axis.text.y = element_text(size = 30), strip.text = element_text(size = 40))
 # To get proper test size etc, save with the following dimensions: 4090 x 1938
+ggsave(filename = paste0(output_folder,"plot1_differentEventTypes.png"), plot = p, units = "in", width = 43, height = 20.4)
+
 
 # Plot 2: How does increasing the proportion of the recombinant sequence affect detection of treelikeness?
+print("Plot 2")
 e = subset(plot2_df, tree1_tree_shape == 'balanced')
 e = subset(e, tree2_tree_shape == 'balanced')
 e = subset(e, tree_age == 1)
@@ -177,15 +80,17 @@ facet_names <- list("neighbour_net" = "Tree-splits proportion","pdm_difference" 
 facet_labeller <- function(variable){
   variable <- facet_names[variable]
 }
-ggplot(e, aes(x = proportion_tree2, y = value)) +
+p <- ggplot(e, aes(x = proportion_tree2, y = value)) +
   geom_point() +
   facet_wrap(~group,scales = "free_y", labeller = labeller(group = facet_labeller)) +
   scale_x_continuous(name = "Proportion of tree 2") +
   ylab("Test statistic value") +
   theme(axis.text.x = element_text(size = 30), axis.title.x = element_text(size = 40), axis.title.y = element_text(size = 40),
         axis.text.y = element_text(size = 30), strip.text = element_text(size = 40))
+ggsave(filename = paste0(output_folder,"plot2_increasingProportionTree2.png"), plot = p, units = "in", width = 43, height = 20.4)
 
 # Plot 3: How does tree age affect detection of treelikeness?
+print("Plot 3")
 e = subset(plot2_df, tree1_tree_shape == 'balanced')
 e = subset(e, tree2_tree_shape == 'balanced')
 e$age = factor(e$tree_age)
@@ -195,35 +100,41 @@ facet_names <- list("neighbour_net" = "Tree-splits proportion","pdm_difference" 
 facet_labeller <- function(variable){
   variable <- facet_names[variable]
 }
-ggplot(e, aes(x = proportion_tree2, y = value, color = age )) +
-  geom_smooth() +
+p <- ggplot(e, aes(x = proportion_tree2, y = value, color = age )) +
+  geom_smooth(size = 3) +
   facet_wrap(~group,scales = "free_y", labeller = labeller(group = facet_labeller)) +
   scale_x_continuous(name = "Proportion of tree 2") +
   ylab("Test statistic value") +
   theme(axis.text.x = element_text(size = 30), axis.title.x = element_text(size = 40), axis.title.y = element_text(size = 40),
-        axis.text.y = element_text(size = 30), strip.text = element_text(size = 40)) +
-  scale_color_viridis(discrete = TRUE, option = "viridis")
+        axis.text.y = element_text(size = 30), strip.text = element_text(size = 40), legend.text = element_text(size = 30),
+        legend.title = element_text(size = 40), legend.key.width = unit(4,"cm"), legend.key.height = unit(2, "cm")) +
+  scale_color_viridis(discrete = TRUE, option = "viridis") +
+  guides(color = guide_legend(title = "Tree depth"))
+ggsave(filename = paste0(output_folder,"plot3_treeAgeWithIncreasingTree2.png"), plot = p, units = "in", width = 48, height = 20.4)
 
 # Plot 4: How does the number of events impact detection of tree likeness?
+print("Plot 4")
 e = subset(plot3_df, tree1_tree_shape == 'balanced')
 e = subset(e, tree2_tree_shape == 'balanced')
 e = subset(e, tree_age == 1)
+e$event_asfactor <- as.factor(e$number_of_events)
 e$group = factor(e$variable,levels = c("PHI_observed","proportion_recombinant_triplets","prop_resolved_quartets","splittable_percentage","pdm_difference","neighbour_net"))
 facet_names <- list("neighbour_net" = "Tree-splits proportion","pdm_difference" = "Summed element-wise difference","PHI_observed" = "PHI","prop_resolved_quartets" = "Proportion of resolved quartets", 
                     "proportion_recombinant_triplets" = "Proportion of recombinant triplets", "splittable_percentage" = "Modified splittable percentage")
 facet_labeller <- function(variable){
   variable <- facet_names[variable]
 }
-ggplot(e, aes(x = event_asfactor, y = value)) +
+p <- ggplot(e, aes(x = event_asfactor, y = value)) +
   geom_boxplot() +
   facet_wrap(~group,scales = "free_y", labeller = labeller(group = facet_labeller)) +
   scale_x_discrete(name = "Number of introgression events") +
   ylab("Test statistic value") +
   theme(axis.text.x = element_text(size = 30), axis.title.x = element_text(size = 40), axis.title.y = element_text(size = 40),
         axis.text.y = element_text(size = 30), strip.text = element_text(size = 40))
-# To get proper test size etc, save with the following dimensions: 4090 x 1938
+ggsave(filename = paste0(output_folder,"plot4_numberOfEvents.png"), plot = p, units = "in", width = 43, height = 20.4)
 
 # Plot 5: How does reciprocity of events influence detection of treelikeness?
+print("Plot 5")
 e = subset(plot3_df, tree1_tree_shape == 'balanced')
 e = subset(e, tree2_tree_shape == 'balanced')
 e = subset(e, tree_age == 1)
@@ -234,16 +145,20 @@ facet_names <- list("neighbour_net" = "Tree-splits proportion","pdm_difference" 
 facet_labeller <- function(variable){
   variable <- facet_names[variable]
 }
-ggplot(e, aes(x = number_of_events, y = value, colour = tree2_event_type)) +
-  geom_smooth() +
+p <- ggplot(e, aes(x = number_of_events, y = value, colour = tree2_event_type)) +
+  geom_smooth(size = 3) +
   facet_wrap(~group,scales = "free_y", labeller = labeller(group = facet_labeller)) +
-  scale_x_discrete(name = "Number of introgression events") +
+  scale_x_continuous(name = "Number of introgression events", breaks = c(0:8), labels = c(0:8)) +
   ylab("Test statistic value") +
   theme(axis.text.x = element_text(size = 30), axis.title.x = element_text(size = 40), axis.title.y = element_text(size = 40),
-        axis.text.y = element_text(size = 30), strip.text = element_text(size = 40)) + 
-  scale_color_viridis(discrete = TRUE, option = "plasma")
+        axis.text.y = element_text(size = 30), strip.text = element_text(size = 40), legend.text = element_text(size = 30),
+        legend.title = element_text(size = 40), legend.key.width = unit(4,"cm"), legend.key.height = unit(2, "cm")) + 
+  scale_color_viridis(discrete = TRUE, option = "plasma", labels = c("Nonreciprocal", "Reciprocal")) + 
+  guides(color = guide_legend(title = "Event type"))
+ggsave(filename = paste0(output_folder,"plot5_ReciprocialAndNonreciprocalEvents.png"), plot = p, units = "in", width = 48, height = 20.4)
 
 # Plot 6: Are the results statistically significant?
+print("Plot 6")
 e = subset(bs_df, tree1_tree_shape == 'balanced')
 e = subset(e, tree2_tree_shape == 'balanced')
 e = subset(e, tree_age == 1)
@@ -254,11 +169,13 @@ facet_names <- list("neighbour_net_p_value" = "Tree-splits \n proportion","pdm_d
 facet_labeller <- function(variable){
   variable <- facet_names[variable]
 }
-ggplot(e, aes(x = value)) +
+p <- ggplot(e, aes(x = value)) +
   geom_histogram(breaks = c(seq(0,1,0.05))) +
   facet_grid(group~proportion_tree2,scales = "free_y", labeller = labeller(group = facet_labeller)) +
   scale_x_continuous(name = "P value") +
   ylab("Count") +
   theme(axis.text.x = element_text(size = 30), axis.title.x = element_text(size = 40), axis.title.y = element_text(size = 40),
-        axis.text.y = element_text(size = 30), strip.text = element_text(size = 40))
+        axis.text.y = element_text(size = 30), strip.text = element_text(size = 40)) +
+  scale_x_continuous(labels = c(0,0.25,0.5,0.75,1))
+ggsave(filename = paste0(output_folder,"plot6_StatisticalSignificance.png"), plot = p, units = "in", width = 43, height = 43)
 
