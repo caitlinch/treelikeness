bd7897e5aa87fbf1de7beb38e465d9928e490a50
caitlinch <24237077+caitlinch@users.noreply.github.com>
2019-04-08 15:56:46 +1000

    Calls trimmed/untrimmed tree proportion for NeighborNet and split decomposition networks

--- code/func_create_alignments.R
+++ code/func_create_alignments.R
@@ -610,11 +610,11 @@ phylo.fixedtrees.run1sim <- function(row, program_paths, tree_folder){
     npds <- normalised.pdm.diff.sum(iqpath = program_paths[["IQTree"]], path = al_file)
     # run normalised pdm difference average (TS2b) (mean of difference of normalised matrix)
     npdm <- normalised.pdm.diff.mean(iqpath = program_paths[["IQTree"]], path = al_file)
-    # run split decomposition (TS3) (split decomposition using splitstree)
-    sd <- SplitsTree.decomposition.statistic(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = al_file,network_algorithm = "split decomposition")
-    # run NeighbourNet (TS3, with neighbour net not split decomposition using splitstree)
-    nn <- SplitsTree.decomposition.statistic(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = al_file,network_algorithm = "neighbournet")
-    # Output pictures of neighbour net and split decomposition networks
+    # Run trimmed and untrimmed versions of the split decomposition and NeighborNet tree proportion
+    sd_untrimmed <- tree.proportion(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = al_file, network_algorithm = "split decomposition", trimmed = FALSE)
+    nn_untrimmed <- tree.proportion(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = al_file, network_algorithm = "neighbournet", trimmed = FALSE)
+    sd_trimmed <- tree.proportion(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = al_file, network_algorithm = "split decomposition", trimmed = TRUE)
+    nn_trimmed <- tree.proportion(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = al_file, network_algorithm = "neighbournet", trimmed = TRUE)
     
     # Collect results
     all_files <- list.files(al_folder) # get a list of all the files
@@ -623,15 +623,15 @@ phylo.fixedtrees.run1sim <- function(row, program_paths, tree_folder){
     proportion_tree_1 <- params_csv$proportion_tree1 # extract proportion of alignment from tree 1
     n_taxa <- length(tree1$tip.label) #ntaxa = number of labels on the tree
     # Make somewhere to store the results
-    df <- data.frame(matrix(nrow=0,ncol=30)) # create an empty dataframe of the correct size
-    op_row <- c(al_file,"phylogenetic_fixedTrees",n_taxa,row[["n_sites"]],row[["tree_age"]],tree1_name,proportion_tree_1,
-                tree2_name,row[["proportion_tree2"]],paste0(row[["id"]],"_",row[["rep"]]),phi_mean,phi_var,phi_obs,phi_sig,num_trips,num_dis,seq_sig,total_q,resolved_q,
-                prop_resolved,partly_resolved_q,unresolved_q,splittable_percentage,npds,npdm,sd,nn) # collect all the information
-    df <- rbind(df,op_row,stringsAsFactors = FALSE) # place row in dataframe
     df_names <- c("alignment","method","n_taxa","n_sites","tree_age","tree1","proportion_tree1","tree2","proportion_tree2","id",
                   "PHI_mean","PHI_variance","PHI_observed","PHI_sig","3SEQ_num_recombinant_triplets","3SEQ_num_distinct_recombinant_sequences","3SEQ_p_value","num_quartets",
                   "num_resolved_quartets","prop_resolved_quartets","num_partially_resolved_quartets","num_unresolved_quartets", "splittable_percentage","pdm_difference",
-                  "pdm_average","split_decomposition", "neighbour_net")
+                  "pdm_average","split_decomposition_untrimmed", "neighbour_net_untrimmed", "split_decomposition_trimmed","neighbour_net_trimmed")
+    df <- data.frame(matrix(nrow=0,ncol=length(df_names))) # create an empty dataframe of the correct size
+    op_row <- c(al_file,"phylogenetic_fixedTrees",n_taxa,row[["n_sites"]],row[["tree_age"]],tree1_name,proportion_tree_1,
+                tree2_name,row[["proportion_tree2"]],paste0(row[["id"]],"_",row[["rep"]]),phi_mean,phi_var,phi_obs,phi_sig,num_trips,num_dis,seq_sig,total_q,resolved_q,
+                prop_resolved,partly_resolved_q,unresolved_q,splittable_percentage,npds,npdm,sd_untrimmed,nn_untrimmed, sd_trimmed,nn_trimmed) # collect all the information
+    df <- rbind(df,op_row,stringsAsFactors = FALSE) # place row in dataframe
     names(df) <- df_names # add names to the df so you know what's what
     write.csv(df,file = results_file)
   }
