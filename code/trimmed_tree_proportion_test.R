e21613790e5b6dd63286ba569ff560c2a2aaf140
caitlinch <24237077+caitlinch@users.noreply.github.com>
2019-04-08 18:08:01 +1000

    Testing delta.plot function with test data from 32 taxa alignment

--- code/trimmed_tree_proportion_test.R
+++ code/trimmed_tree_proportion_test.R
@@ -9,6 +9,7 @@ tree_folder <- paste0(maindir,"trees/")
 
 # Set filepaths
 al_path <- "/Users/caitlincherryh/Documents/Honours/TestAlignmentResults/5_newTS/divergent_alignment.nexus"
+al2_path <- "/Users/caitlincherryh/Documents/Honours/TestAlignmentResults/5_newTS/32_8event.fasta"
 results_folder <- "/Users/caitlincherryh/Documents/Honours/TestAlignmentResults/5_newTS/"
 main_dir <- "/Users/caitlincherryh/Documents/Honours/TestAlignmentResults/5_newTS/"
 exec_folder <- "/Users/caitlincherryh/Documents/Honours/Executables/"
@@ -17,5 +18,11 @@ exec_paths <- paste0(exec_folder,exec_paths)
 names(exec_paths) <- c("3seq","IQTree","Phi","SimBac","SplitsTree")
 
 # Call decomposition statistic
-x <- tree.proportion(iqpath = exec_paths[["IQTree"]], splitstree_path = exec_paths[["SplitsTree"]], path = al_path, network_algorithm = "neighbournet", trimmed = FALSE)
-y <- tree.proportion(iqpath = exec_paths[["IQTree"]], splitstree_path = exec_paths[["SplitsTree"]], path = al_path, network_algorithm = "neighbournet", trimmed = TRUE)
+x <- tree.proportion(iqpath = exec_paths[["IQTree"]], splitstree_path = exec_paths[["SplitsTree"]], path = al2_path, network_algorithm = "neighbournet", trimmed = FALSE)
+y <- tree.proportion(iqpath = exec_paths[["IQTree"]], splitstree_path = exec_paths[["SplitsTree"]], path = al2_path, network_algorithm = "neighbournet", trimmed = TRUE)
+
+# Call delta plot function
+mldm <- mldist.pdm(al2_path)
+pdm <- as.matrix(mldm)
+pdm[upper.tri(pdm)] <- t(pdm)[upper.tri(pdm)] # copy across upper diagonal
+z <- delta.plot(pdm, k = 20, plot = TRUE, which = 1:2)
