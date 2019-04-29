6bd38d04c52bf3616de112a317485a56e4c9721f
caitlinch <24237077+caitlinch@users.noreply.github.com>
2019-04-29 13:34:12 +1000

    Extract n_taxa and n_char from nexus file, use for likelihood mapping

--- code/func_BA.R
+++ code/func_BA.R
@@ -1,3 +1,21 @@
 empirical.runTS <- function(alignment_path, program_paths){
-  # output_dataframe: dataset, loci name, number of taxa, number of characters, test statistic values 
+  # Open the nexus file and get the number of taxa and the number of characters 
+  n <- read.nexus.data(alignment_path)
+  n_taxa <- length(n)
+  n_char <- length(unlist(n[1]))
+  
+  # Run IQ-tree on the alignment (if it hasn't already been run), and get the likelihood mapping results
+  call.IQTREE.quartet(program_paths[["IQTree"]],alignment_path,n_taxa)
+  
+  # output_dataframe: dataset, loci name, number of taxa, number of characters, test statistic values
+}
+
+empirical.parametric.bootstrap <- function(){
+  # If it hasn't already been run, call and run IQTree
+  call.IQTREE(program_paths["IQTree"],alignment_path)
+  
+  #Extract the parameters from the .iqtree log file.
+  params <- get.simulation.parameters(paste0(alignment_path,".iqtree"))
+  
+  # output_dataframe: dataset, loci name, number of taxa, number of characters, test statistic values
 }
