cefd6eea22a4095b62d7abec64e4e55d9f9a6a81
caitlinch <24237077+caitlinch@users.noreply.github.com>
2019-04-29 15:42:46 +1000

    Added bootstrap replicate id to output df (can now use same functions for bootstrap replicates OR for empirical alignments

--- code/func_BA.R
+++ code/func_BA.R
@@ -4,7 +4,7 @@ library(phytools)
 library(ape)
 library(phangorn)
 
-empirical.runTS <- function(alignment_path, program_paths){
+empirical.runTS <- function(alignment_path, program_paths, bootstrap_id = FALSE){
   # extract the alignment folder from the alignment path
   alignment_folder <- paste0(dirname(alignment_path),"/")
   # Extract the dataset name (basename of alignment folder: element after last "/" in alignment_folder)
@@ -126,20 +126,29 @@ empirical.runTS <- function(alignment_path, program_paths){
   sd_trimmed <- tree.proportion(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = new_nexus_file, network_algorithm = "split decomposition", trimmed = TRUE, tree_path = initial_iqtree_tree, run_IQTREE = FALSE)
   nn_trimmed <- tree.proportion(iqpath = program_paths[["IQTree"]], splitstree_path = program_paths[["SplitsTree"]], path = new_nexus_file, network_algorithm = "neighbournet", trimmed = TRUE, tree_path = initial_iqtree_tree, run_IQTREE = FALSE)
   
-  # output_dataframe: dataset, loci name, number of taxa, number of characters, test statistic values
+  # Name the test statistics file (if it's a  bootstrap replicate, add the replicate number!)
+  # Create the variable for bootstrap replicate
+  if (bootstrap_id == FALSE){
+    results_file <- paste0(alignment_folder,loci_name,"_testStatistics.csv")
+    rep_id = "empiricalAlignment"
+  } else {
+    results_file <- paste0(alignment_folder,loci_name,"_bootstrapReplicate",bootstrap_id,"_testStatistics.csv")
+    rep_id <- paste0("bootstrapReplicate",bootstrap_id)
+  }
+  
   # Make somewhere to store the results
-  df_names <- c("dataset","loci","n_taxa","n_sites","alignment_file",
+  df_names <- c("dataset","loci","bootstrap_replicate_id","n_taxa","n_sites","alignment_file",
                 "PHI_mean","PHI_variance","PHI_observed","PHI_sig","3SEQ_num_recombinant_triplets","3SEQ_num_distinct_recombinant_sequences","3SEQ_p_value","num_quartets",
                 "num_resolved_quartets","prop_resolved_quartets","num_partially_resolved_quartets","num_unresolved_quartets", "splittable_percentage","pdm_difference",
                 "pdm_average","split_decomposition_untrimmed", "neighbour_net_untrimmed", "split_decomposition_trimmed","neighbour_net_trimmed","mean_delta_q","median_delta_q","mode_delta_q")
   df <- data.frame(matrix(nrow=0,ncol=length(df_names))) # create an empty dataframe of the correct size
-  op_row <- c(dataset,loci_name,n_taxa,n_char,alignment_path,
+  op_row <- c(dataset,loci_name,rep_id,n_taxa,n_char,alignment_path,
               phi_mean,phi_var,phi_obs,phi_sig,num_trips,num_dis,seq_sig,total_q,resolved_q,
               prop_resolved,partly_resolved_q,unresolved_q,splittable_percentage,npds,npdm,sd_untrimmed,nn_untrimmed,
               sd_trimmed,nn_trimmed,mean_dq,median_dq,mode_dq) # collect all the information
   df <- rbind(df,op_row,stringsAsFactors = FALSE) # place row in dataframe
   names(df) <- df_names # add names to the df so you know what's what
-  results_file <- paste0(alignment_folder,loci_name,"_testStatistics.csv")
+  
   write.csv(df,file = results_file)
 }
 
