fb16da4a4a950c9be686206bba3b9f0438e71446
caitlinch <24237077+caitlinch@users.noreply.github.com>
2019-05-31 13:48:02 +1000

    Order ds from least to most taxa

--- code/5_TestStatistics_EmpiricalData.R
+++ code/5_TestStatistics_EmpiricalData.R
@@ -1,5 +1,6 @@
 # Script to apply test statistics and parametric bootstrap to empirical data sets in the BenchmarkAlignments database
 
+print("opening packages")
 library(ape)
 library(parallel)
 library(phangorn)
@@ -7,6 +8,8 @@ library(phytools)
 library(seqinr)
 library(stringr)
 library(TreeSim)
+
+print("initialising namespace")
 
 # run_location = "mac"
 run_location = "soma"
@@ -52,13 +55,14 @@ source(paste0(maindir,"code/func_process_data.R"))
 source(paste0(maindir,"code/func_parametric_bootstrap.R"))
 
 # Extract the file names of the alignments
+print("extracting alignments")
 files <- list.files(BA_dir,recursive = TRUE) # list all files
 als <- paste0(BA_dir,files[grep(".nex",files)]) # get all the nexus files
 als <- als[!als %in% als[grep(".nex.",als)]] # remove all non alignment files to leave only alignments
 als <- als[!als %in% als[grep("bootstrapReplicate",als)]] # remove all bootstrap alignments (if any present)
 als <- als[!als %in% als[grep("alignment.nex",als)]] # remove full alignments (only want to run per loci)
-# als <- sort(als,decreasing = TRUE) # reverse list so it runs the HUGE mammal dataset first
-order_to_run <- c("Worobey_2014h","Worobey_2014a","Worobey_2014f","Worobey_2014g","Worobey_2014e","Worobey_2014b",
+# Nucleotide datasets ordered from smallest to largest number of partitions in original dataset
+partition_order <- c("Worobey_2014h","Worobey_2014a","Worobey_2014f","Worobey_2014g","Worobey_2014e","Worobey_2014b",
                   "Worobey_2014c","Worobey_2014d","Looney_2016","Anderson_2013","Devitt_2013","Seago_2011","Siler_2013",
                   "Cognato_2001","Brown_2012","Wood_2012","Bergsten_2013","Murray_2013","Kawahara_2013","Sauquet_2011",
                   "Day_2013","Sharanowski_2011","Tolley_2013","Dornburg_2012","Unmack_2013","Rightmyer_2013","Horn_2014",
@@ -66,13 +70,25 @@ order_to_run <- c("Worobey_2014h","Worobey_2014a","Worobey_2014f","Worobey_2014g
                   "Fong_2012","Cannon_2016_dna","Faircloth_2013","Moyle_2016","Leache_2015","Branstetter_2017",
                   "Crawford_2012","Smith_2014","Meiklejohn_2016","McCormack_2013","Richart_2015","Prebus_2017",
                   "Ran_2018_dna","Wu_2018_dna")
+# Nucleotide datasets ordered from smallest to largest number of taxa
+taxa_order <- c('Richart_2015','Smith_2014','Crawford_2012','Leache_2015','Meiklejohn_2016','Faircloth_2013',
+                'McCormack_2013','Wood_2012','Bergsten_2013','Ran_2018_dna','Brown_2012','Cognato_2001',
+                'Dornburg_2012','Prebus_2017','Sauquet_2011','Broughton_2013','Siler_2013','Devitt_2013',
+                'Kawahara_2013','Cannon_2016_dna','Lartillot_2012','Oaks_2011','Wu_2018_dna','Worobey_2014c',
+                'Rightmyer_2013','Seago_2011','Moyle_2016','Fong_2012','Unmack_2013','Sharanowski_2011',
+                'Anderson_2013','Worobey_2014a','Day_2013','Branstetter_2017','Wainwright_2012','Horn_2014',
+                'Tolley_2013','Reddy_2017','Murray_2013','Worobey_2014g','Worobey_2014b','Worobey_2014f',
+                'Worobey_2014e','Worobey_2014h','Worobey_2014d','Near_2013','Looney_2016','Pyron_2011')
+# Sort the list of alignments by the desired order
+print("sorting alignments")
 als_ordered <- c()
-for (ds in order_to_run){
+for (ds in taxa_order){
   als_ordered <- c(als_ordered,als[grep(ds,als)])
 }
 als <- als_ordered
 
 # Calculate the test statistics and run the bootstraps
+print("starting analysis")
 # To run for one alignment: empirical.bootstraps.wrapper(empirical_alignment_path = empirical_alignment_path, program_paths = program_paths, number_of_replicates = 9)
 if (run_location=="soma"){
   lapply(als,empirical.bootstraps.wrapper, program_paths = exec_paths, number_of_replicates = 99) 
